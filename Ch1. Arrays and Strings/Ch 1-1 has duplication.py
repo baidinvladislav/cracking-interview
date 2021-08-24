92aa3203c4edb59ba608b6bf5d2915cfd70c7355
Vladislav Baydin <vbaydin96@gmail.com>
2021-08-24 22:31:32 +0300

    Resolved 1.1 problem

--- Ch1. Arrays and Strings/Ch 1-1 has duplication.py
+++ Ch1. Arrays and Strings/Ch 1-1 has duplication.py
@@ -1,15 +1,15 @@
-def noDupe(string):
-  a={}
-  for char in string:
-    if char in a:
-      return False
-    else:
-      a[char] = 1
-  return True
+def no_duplicate(string):
+    storage = {}
+    for character in string:
+        if character in storage:
+            print(f'String -{string}- contains duplicates of -{character}- character')
+            return False
+        else:
+            storage[character] = 1
+    print(f'String -{string}- does not contain any character duplicates')
+    return True
 
 
-print (noDupe('aaaaaa'))
-print (noDupe('abcdefg'))
-print (noDupe(''))
-print (noDupe('abcdefghijklmnopqrstuvwxyzz'))
-print (noDupe('abcdefghijklmnopqrstuvwxyz'))
+no_duplicate('Hello')  # False
+no_duplicate('World')  # True
+no_duplicate('Python')  # True
