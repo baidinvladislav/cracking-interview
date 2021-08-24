0f0def51f136aa9d8c5986dd7c40a0881ab1602f
Vladislav Baydin <vbaydin96@gmail.com>
2021-08-24 22:09:40 +0300

    Initial commit

---
+++ Ch1. Arrays and Strings/Ch 1-1 has duplication.py
@@ -0,0 +1,15 @@
+def noDupe(string):
+  a={}
+  for char in string:
+    if char in a:
+      return False
+    else:
+      a[char] = 1
+  return True
+
+
+print (noDupe('aaaaaa'))
+print (noDupe('abcdefg'))
+print (noDupe(''))
+print (noDupe('abcdefghijklmnopqrstuvwxyzz'))
+print (noDupe('abcdefghijklmnopqrstuvwxyz'))
