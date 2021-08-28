6be3057387f5360d80a62076bb19c12321f30ea6
Vladislav Baydin <vbaydin96@gmail.com>
2021-08-28 11:46:54 +0300

    Created a graph

--- Data structures/Graph.py
+++ Data structures/Graph.py
@@ -10,4 +10,27 @@ class Vertex:
 
 
 class Graph:
-	pass
+	vertices = {}
+
+	def add_vertex(self, vertex):
+		if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+			self.vertices[vertex.name] = vertex
+			return True
+		else:
+			return False
+
+	def add_edge(self, u, v):
+		if u in self.vertices and v in self.vertices:
+			for key, value in self.vertices.items():
+				if key == u:
+					value.add_neighbor(v)
+				if key == v:
+					value.add_neighbor(u)
+			return True
+		else:
+			return False
+
+	def print_graph(self):
+		for key in sorted(list(self.vertices.keys())):
+			print(key + str(self.vertices[key].neighbors))
+
