a18cf2781b5a3e3bfb03198a9f7f9808298fced5
Vladislav Baydin <vbaydin96@gmail.com>
2021-09-02 23:25:23 +0300

    Resolved problem #6

---
+++ Problems/1. Arrays and Strings/6.string compression.py
@@ -0,0 +1,29 @@
+# Write a function which compressions strings
+
+
+def to_compress(string):
+    storage = {}
+    for char in string:
+        if char in storage:
+            storage[char] += 1
+        else:
+            storage[char] = 1
+
+    final_str = ''
+    for key, value in storage.items():
+        container = key + str(value)
+        final_str += container
+
+    return final_str
+
+
+if __name__ == '__main__':
+    test_cases = [
+        'aawwweeerrrtt',
+        'xxxzzzasdgvfff',
+        'qwerttyu',
+        'poiybsasaasdscznxg'
+    ]
+
+    for string in test_cases:
+        print(to_compress(string))
