cda8b62e78c88ab1aa7854c9d3e0faaae95308c9
Vladislav Baydin <vbaydin96@gmail.com>
2021-09-04 11:05:36 +0300

    Resolved problem #7

---
+++ Problems/1. Arrays and Strings/7.matrix 90 degrees.py
@@ -0,0 +1,28 @@
+# Write a function which rotates the matrix 90 degrees
+
+
+def rotate_matrix(matrix):
+    n = len(matrix)
+    for layer in range(n // 2):
+        first, last = layer, n - layer - 1
+        for i in range(first, last):
+            top = matrix[layer][i]
+
+            matrix[layer][i] = matrix[-i - 1][layer]
+
+            matrix[-i - 1][-i - 1] = matrix[i][- layer - 1]
+
+            matrix[i][- layer - 1] = top
+
+    return matrix
+
+
+matrix = [
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25]
+        ]
+
+print(rotate_matrix(matrix))
