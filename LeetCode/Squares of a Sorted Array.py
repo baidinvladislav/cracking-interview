0dc88195f74fec958de328471f86d7556fb9dd80
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-09-15 16:46:01 +0300

    Resolved LeetCode Theory task

---
+++ LeetCode/Squares of a Sorted Array.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums sorted in non-decreasing order,
+return an array of the squares of each number sorted in non-decreasing order.
+"""
+from _ast import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        squared_nums = [i * i for i in nums]
+        squared_nums.sort()
+        return squared_nums
