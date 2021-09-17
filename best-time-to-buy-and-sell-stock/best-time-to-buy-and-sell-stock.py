99cd7fa381b4080c7b3d31a984dda42e873ffbca
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-09-17 15:30:13 +0300

    Moved auto-committed problems to single repository

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxProfit(self, prices):
-        max_profit, min_price = 0, float('inf')
-        for price in prices:
-            min_price = min(min_price, price)
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
-        return max_profit
