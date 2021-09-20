4f6d91b4d8ead846ca7d310ce4b88e440bba6708
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-09-20 17:23:51 +0300

    More better solution #88

--- LeetCode/88. Merge Sorted Array.py
+++ LeetCode/88. Merge Sorted Array.py
@@ -10,17 +10,22 @@ To accommodate this, nums1 has a length of m + n,
 where the first m elements denote the elements that should be merged,
 and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 """
-from typing import List
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        # Write the elements of num2 into the end of nums1.
-        for i in range(n):
-            nums1[i + m] = nums2[i]
+    def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        while n > 0:
+            nums1[n-1] = nums2[n-1]
+            n -= 1
 
-        # Sort nums1 list in-place.
-        nums1.sort()
+        print(nums1)
+
+
+Solution().merge([1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3)
