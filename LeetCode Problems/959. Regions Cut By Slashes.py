d068f7b869671fce4ae8d25280b18a4bbaed4cf2
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-07 20:04:28 +0300

    Resolved LeetCode #959

---
+++ LeetCode Problems/959. Regions Cut By Slashes.py
@@ -0,0 +1,43 @@
+"""
+An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\',
+or blank space ' '. These characters divide the square into contiguous regions.
+
+Given the grid grid represented as a string array, return the number of regions.
+
+Note that backslash characters are escaped, so a '\' is represented as '\\'.
+"""
+
+
+class Solution:
+    def regionsBySlashes(self, grid):
+        f = {}
+
+        def find(x):
+            f.setdefault(x, x)
+            if x != f[x]:
+                f[x] = find(f[x])
+            return f[x]
+
+        def union(x, y):
+            f[find(x)] = find(y)
+
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if i:
+                    union((i - 1, j, 2), (i, j, 0))
+
+                if j:
+                    union((i, j - 1, 1), (i, j, 3))
+
+                if grid[i][j] != "/":
+                    union((i, j, 0), (i, j, 1))
+                    union((i, j, 2), (i, j, 3))
+
+                if grid[i][j] != "\\":
+                    union((i, j, 3), (i, j, 0))
+                    union((i, j, 1), (i, j, 2))
+
+        return len(set(map(find, f)))
+
+
+print(Solution().regionsBySlashes(grid=[" /", "/ "]))
