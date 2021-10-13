73d64e72b2bde2fc80d4ac067cb90fabc0a4d244
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-13 23:58:00 +0300

    Added O(1) memory solution

--- LeetCode Problems/Find Numbers with Even Number of Digits.py
+++ LeetCode Problems/Find Numbers with Even Number of Digits.py
@@ -4,6 +4,7 @@ Given an array nums of integers, return how many of them contain an even number 
 from typing import List
 
 
+# with additional memory
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         buf = []
@@ -11,3 +12,22 @@ class Solution:
             if len(str(i)) % 2 == 0:
                 buf.append(i)
         return len(buf)
+
+    
+print(Solution().findNumbers(nums = [12,345,2,6,7896]))
+
+
+# memory O(1)
+class Solution1:
+    def findNumbers(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        
+        for i in range(n):
+            if len(str(nums[i])) % 2 == 0:
+                count += 1
+                
+        return count
+
+    
+print(Solution1().findNumbers(nums = [12,345,2,6,7896]))
