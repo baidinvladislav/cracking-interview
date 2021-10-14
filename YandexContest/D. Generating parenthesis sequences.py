a5d41dcb8dd4898d867053143e3bcbb6617266ed
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-14 21:44:19 +0300

    Resolved D problem

--- YandexContest/D. Generating parenthesis sequences.py
+++ YandexContest/D. Generating parenthesis sequences.py
@@ -11,26 +11,24 @@
 
 https://sohabr.net/habr/post/420605/
 https://habr.com/ru/post/191298/
+https://hashsum.ru/generaciya-skobochnyx-posledovatelnostej/
 """
-import random
 
 
-class Solution:
-    def generate_sequence(self):
-        import sys
-        n = sys.stdin.readline().strip()
-
-        if not 0 <= len(n) <= 11:
-            print(f'Incorrect input data')
-            return
-
-        for i in range(int(n)):
-            seq = ['(', ')'] * int(n)
-            random.shuffle(seq)
-            print(seq)
-
-    def correct(self, seq):
-        pass
+import sys
 
 
-Solution().generate_sequence()
+n = int(sys.stdin.readline())
+
+
+def bracket(count, s='', left=0, right=0):
+    if left == count and right == count:
+        print(s)
+    else:
+        if left < count:
+            bracket(count, s + '(', left+1, right)
+        if right < left:
+            bracket(count, s + ')', left, right+1)
+
+
+bracket(n)
