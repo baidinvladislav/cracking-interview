4b39856b42984b0aec01b4eb824144a3d4158be3
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-20 00:57:13 +0300

    Cleaned code

--- LeetCode Problems/Red problems/1038. Binary Search Tree to Greater Sum Tree.py
+++ LeetCode Problems/Red problems/1038. Binary Search Tree to Greater Sum Tree.py
@@ -1,104 +1,38 @@
 """
-Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original
-BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such
+that every key of the original BST is changed to the original key plus the sum
+of all keys greater than the original key in BST.
 
 As a reminder, a binary search tree is a tree that satisfies these constraints:
 
-The left subtree of a node contains only nodes with keys less than the node's key.
-The right subtree of a node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees must also be binary search trees.
+* The left subtree of a node contains only nodes with keys less than the node's key.
+* The right subtree of a node contains only nodes with keys greater than the node's key.
+* Both the left and right subtrees must also be binary search trees.
 """
-from collections import deque
+from Algorithms.leetcode_tree import TreeNode, buildTree, print_tree
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    def __repr__(self):
-        return str(self.val)
-
-
-# geeks for geeks
-def buildTree(ip):
-    # Create the root of the tree
-    root = TreeNode(int(ip[0]))
-    size = 0
-    q = deque()
-
-    # Push the root to the queue
-    q.append(root)
-    size = size + 1
-
-    # Starting from the second element
-    i = 1
-    while size > 0 and i < len(ip):
-        # Get and remove the front of the queue
-        currNode = q[0]
-        q.popleft()
-        size = size - 1
-
-        # Get the current node's value from the string
-        currVal = ip[i]
-
-        # If the left child is not null
-        if currVal != None:
-            # Create the left child for the current node
-            currNode.left = TreeNode(int(currVal))
-
-            # Push it to the queue
-            q.append(currNode.left)
-            size = size + 1
-        # For the right child
-        i = i + 1
-        if i >= len(ip):
-            break
-        currVal = ip[i]
-
-        # If the right child is not null
-        if currVal != None:
-            # Create the right child for the current node
-            currNode.right = TreeNode(int(currVal))
-
-            # Push it to the queue
-            q.append(currNode.right)
-            size = size + 1
-        i = i + 1
-    return root
-
-
-def print_tree(node, level=0):
-    if node is not None:
-        print_tree(node.right, level=level + 1)
-        print(3 * ' ' * level + str(node.val))
-        print_tree(node.left, level=level + 1)
-
-
+# date: 19.10.21
 class Solution:
+
+    def _inorder_traversal(self, node):
+        if node is not None:
+            self._inorder_traversal(node.left)
+            self.inorder.append(node)
+            self._inorder_traversal(node.right)
+
     def bstToGst(self, root: TreeNode) -> TreeNode:
-
         self.inorder = []
+        self._inorder_traversal(root)
 
-        # O(n)
-        def _inorder_traversal(node):
-            if node is not None:
-                _inorder_traversal(node.left)
-                self.inorder.append(node)
-                _inorder_traversal(node.right)
-
-        _inorder_traversal(root)
-
-        # O(n)
         n = len(self.inorder)
         cur = self.inorder[-1].val
         cumsum = [cur]
+
         for i in range(n - 2, -1, -1):
             cur = cur + self.inorder[i].val
             cumsum.append(cur)
 
-        # O(n)
         for i, node in enumerate(self.inorder):
             node.val = node.val + cumsum[n - 1 - i]
 
@@ -108,6 +42,6 @@ class Solution:
 if __name__ == '__main__':
     root_arr = [4, 1, 6, 0, 2, 5, 7, None, None, None, 3, None, None, None, 8]
     root = buildTree(root_arr)
-    print_tree(root)
+    # print_tree(root)
     print(Solution().bstToGst(root))
-    print_tree(root)
+    # print_tree(root)
