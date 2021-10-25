ec46efb6ff407477d7dc1c845598a0ad3644019b
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-25 15:59:19 +0300

    Update solution

--- LeetCode Problems/Yellow problems/101. Symmetric Tree.py
+++ LeetCode Problems/Yellow problems/101. Symmetric Tree.py
@@ -9,30 +9,28 @@ from Algorithms.leetcode_tree import TreeNode, buildTree
 
 class Solution:
 
-    def _in_order_traversal_right(self, node):
+    def _in_order_traversal_left(self, node, left_arr):
         if node is not None:
-            self._in_order_traversal_right(node.right)
-            self.in_order_arr_right.append(node.val)
-            self._in_order_traversal_right(node.left)
+            self._in_order_traversal_left(node.left, left_arr)
+            left_arr.append(node.val)
+            self._in_order_traversal_left(node.right, left_arr)
+        left_arr.append(None)
 
-        self.in_order_arr_right.append(None)
-
-    def _in_order_traversal_left(self, node):
+    def _in_order_traversal_right(self, node, right_arr):
         if node is not None:
-            self._in_order_traversal_left(node.left)
-            self.in_order_arr_left.append(node.val)
-            self._in_order_traversal_left(node.right)
-
-        self.in_order_arr_left.append(None)
+            self._in_order_traversal_right(node.right, right_arr)
+            right_arr.append(node.val)
+            self._in_order_traversal_right(node.left, right_arr)
+        right_arr.append(None)
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        self.in_order_arr_left = []
-        self.in_order_arr_right = []
+        left = []
+        right = []
 
-        self._in_order_traversal_left(root.left)
-        self._in_order_traversal_right(root.right)
+        self._in_order_traversal_left(root.left, left)
+        self._in_order_traversal_right(root.right, right)
 
-        if self.in_order_arr_left == self.in_order_arr_right:
+        if left == right:
             return True
         else:
             return False
