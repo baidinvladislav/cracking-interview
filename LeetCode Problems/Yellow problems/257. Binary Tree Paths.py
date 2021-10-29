3829673242a9e118f6ed50f28c44fe49fc5a03f2
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-29 14:31:08 +0300

    Resolved LeetCode #257

--- LeetCode Problems/Yellow problems/257. Binary Tree Paths.py
+++ LeetCode Problems/Yellow problems/257. Binary Tree Paths.py
@@ -4,12 +4,15 @@ Given the root of a binary tree, return all root-to-leaf paths in any order.
 A leaf is a node with no children.
 """
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 from typing import Optional, List
 
 from Algorithms.leetcode_tree import TreeNode, buildTree
@@ -17,24 +20,23 @@ from Algorithms.leetcode_tree import TreeNode, buildTree
 
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root: return []
+        if not root.left and not root.right: return [str(root.val)]
 
-        def recursion(root, s):
+        self.res = []
 
-            if root is None:
-                return s
-            else:
-                s += str(root.val)
+        def dfs(root, path):
+            if not root:
+                return
+            path += str(root.val)
+            if not root.left and not root.right:
+                self.res.append(path)
+                return
+            dfs(root.left, path + '->')
+            dfs(root.right, path + '->')
 
-            s += str(recursion(root.left, paths))
-            s += str(recursion(root.right, paths))
-
-
-        s = ''
-        s += str(root.val)
-        paths = []
-
-        s += str(recursion(root.right, s))
-        return s
+        dfs(root, "")
+        return self.res
 
 
 root_arr = [1, 2, 3, None, 5]
