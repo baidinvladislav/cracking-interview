98fbaeb010e46d8b2434bde0871cf7935194376c
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-02 17:07:32 +0300

    Added BFS by Lee215

--- Algorithms/bfs_built-in.py
+++ Algorithms/bfs_built-in.py
@@ -1,6 +1,27 @@
 import collections
 
 
+class Solution:
+    def bfs_lee215(self, root, k):
+        if not root:
+            return False
+
+        queue, visited = [root], set()
+        for node in queue:
+            if k - node.val in visited:
+                return True
+
+            visited.add(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        return False
+
+
 def bfs(root):
     deque = collections.deque()
 
