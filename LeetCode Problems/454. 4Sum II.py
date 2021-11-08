fe06df1a10e059ce3a10c0b936058a3036daa96a
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-08 18:03:31 +0300

    Moved to red list

---
+++ LeetCode Problems/454. 4Sum II.py
@@ -0,0 +1,16 @@
+"""
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n,
+return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+"""
+from typing import List
+
+
+# can not resolve it 08.11.21
+class Solution:
+    pass
+
+
+print(Solution().fourSumCount(nums1=[1, 2], nums2=[-2, -1], nums3=[-1, 2], nums4=[0, 2]))
