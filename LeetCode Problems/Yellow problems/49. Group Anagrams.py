1ca0c74ad3994b7a99db97eeb201da6e7e2dc3bb
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-09 16:59:16 +0300

    Lost blank lines

--- LeetCode Problems/Yellow problems/49. Group Anagrams.py
+++ LeetCode Problems/Yellow problems/49. Group Anagrams.py
@@ -4,6 +4,8 @@ Given an array of strings strs, group the anagrams together. You can return the 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 typically using all the original letters exactly once.
 """
+
+
 import collections
 
 
