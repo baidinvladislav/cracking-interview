2c829b684375437f1264b4eab7d2fb68e9905c79
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-10 16:52:40 +0300

    Resolved problem #125 from Yandex track

---
+++ Yandex LeetCode/125. Valid Palindrome.py
@@ -0,0 +1,20 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+and removing all non-alphanumeric characters, it reads the same forward and backward.
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        filtered_chars = filter(lambda ch: ch.isalnum(), s)
+        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
+
+        filtered_chars_list = list(lowercase_filtered_chars)
+        return filtered_chars_list == filtered_chars_list[::1]
+
+
+print(Solution().isPalindrome(s="race a car"))
