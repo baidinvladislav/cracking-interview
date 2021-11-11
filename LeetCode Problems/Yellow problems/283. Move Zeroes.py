ec889c0b08f6f1c0346a950a38bb42dce2bb4f09
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-11 14:10:31 +0300

    Removed unnecessary variable

--- LeetCode Problems/Yellow problems/283. Move Zeroes.py
+++ LeetCode Problems/Yellow problems/283. Move Zeroes.py
@@ -8,13 +8,12 @@ Note that you must do this in-place without making a copy of the array.
 
 class Solution:
     def moveZeroes(self, nums):
-        pos = 0
+        pointer = 0
 
         for i in range(len(nums)):
-            el = nums[i]
-            if el != 0:
-                nums[pos], nums[i] = nums[i], nums[pos]
-                pos += 1
+            if nums[i] != 0:
+                nums[pointer], nums[i] = nums[i], nums[pointer]
+                pointer += 1
 
 
 print(Solution().moveZeroes(nums=[0, 1, 0, 3, 12]))
