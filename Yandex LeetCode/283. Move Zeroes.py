6a9d33deadd1771222320531ca0cda199d1a5033
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-11 13:18:09 +0300

    Resolved problem #283 from Yandex track

---
+++ Yandex LeetCode/283. Move Zeroes.py
@@ -0,0 +1,19 @@
+"""
+Given an integer array nums, move all 0's to the end
+of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+"""
+
+
+class Solution:
+    def moveZeroes(self, nums):
+        pos = 0
+
+        for i in range(len(nums)):
+            el = nums[i]
+            if el != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+
+        return nums
