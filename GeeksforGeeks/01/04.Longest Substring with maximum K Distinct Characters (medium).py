6e6962bf397b1cb353ac1238bc2f44201b018279
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-15 16:47:34 +0300

    Added docstring

--- GeeksforGeeks/01/04.Longest Substring with maximum K Distinct Characters (medium).py
+++ GeeksforGeeks/01/04.Longest Substring with maximum K Distinct Characters (medium).py
@@ -4,6 +4,20 @@ Given a string, find the length of the longest substring in it with no more than
 
 
 def longest_substring_with_k_distinct(str1, k):
+    """
+    1. Initialize `window_start` to tracking beginning of window.
+    2. Initialize `max_length` to keeping the max length of sub-array.
+    3. Initialize `char_frequency` to keeping the amount of frequency elements.
+    4. Looping to input string and adding char to `char_frequency` like key and amount frequency as value.
+    5. If length of `char_frequency` greater than `K` then decrease their value by 1.
+    6. If value of char equal 0 then delete this key from `char_frequency`.
+    7. Increase `window_start` by 1.
+    8. Update `max_length` if window_end - window_start + 1 greater than `max_length`.
+
+    :param str1: `str` input string.
+    :param k: `int` amount values inside sub-array.
+    :return: `int` max_length max length of sub-array distinct K elements.
+    """
     window_start = 0
     max_length = 0
     char_frequency = {}
