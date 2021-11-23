f42eb8fdb6500b61c4691f24447116ac1f967936
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-23 21:24:33 +0300

    Added my comments

--- Patterns for Coding Questions/01.Sliding Window/07.Longest Substring with Same Letters after Replacement (hard).py
+++ Patterns for Coding Questions/01.Sliding Window/07.Longest Substring with Same Letters after Replacement (hard).py
@@ -8,25 +8,25 @@ def length_of_longest_substring(str1, k):
     window_start, max_length, max_repeat_letter_count = 0, 0, 0
     frequency_map = {}
 
-    # Try to extend the range [window_start, window_end]
     for window_end in range(len(str1)):
+        # add character to hash-map
         right_char = str1[window_end]
         if right_char not in frequency_map:
             frequency_map[right_char] = 0
         frequency_map[right_char] += 1
-        max_repeat_letter_count = max(
-            max_repeat_letter_count, frequency_map[right_char])
+        # tracking the most repeatable character
+        max_repeat_letter_count = max(max_repeat_letter_count, frequency_map[right_char])
 
-        # Current window size is from window_start to window_end, overall we have a letter which is
-        # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
-        # repeating 'max_repeat_letter_count' times and the remaining letters we should replace.
-        # if the remaining letters are more than 'k', it is the time to shrink the window as we
-        # are not allowed to replace more than 'k' letters
-        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+        window_size = window_end - window_start + 1
+        diff = window_size - max_repeat_letter_count
+        # if window size includes unique symbols greater then number of available replacement
+        if diff > k:
+            # we'll shrink the window
             left_char = str1[window_start]
             frequency_map[left_char] -= 1
             window_start += 1
 
+        # update max length if it greater then it was
         max_length = max(max_length, window_end - window_start + 1)
     return max_length
 
