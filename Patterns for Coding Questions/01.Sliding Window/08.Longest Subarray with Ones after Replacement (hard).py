f1a1dcef4240e07bb571ac8c85983c6c8c3f8f82
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-11-25 17:20:02 +0300

    Resolved 08

---
+++ Patterns for Coding Questions/01.Sliding Window/08.Longest Subarray with Ones after Replacement (hard).py
@@ -0,0 +1,28 @@
+"""
+Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s,
+find the length of the longest contiguous subarray having all 1s.
+"""
+
+
+def length_of_longest_substring(arr, k):
+    ones, window_start, max_length = 0, 0, 0
+
+    for window_end in range(len(arr)):
+        if arr[window_end] == 1:
+            ones += 1
+
+        if window_end - window_start + 1 - ones > k:
+            if arr[window_start] == 1:
+                ones -= 1
+            window_start += 1
+        max_length = max(window_end - window_start + 1, max_length)
+
+    return max_length
+
+
+def main():
+    print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+    print(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+
+
+main()
