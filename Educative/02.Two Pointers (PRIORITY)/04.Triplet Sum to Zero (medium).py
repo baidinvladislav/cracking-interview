90a6de65e889aa8d6dfcb77cc02b0249ef45b033
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-01 16:43:59 +0300

    Repeated this problem

--- Educative/02.Two Pointers (PRIORITY)/04.Triplet Sum to Zero (medium).py
+++ Educative/02.Two Pointers (PRIORITY)/04.Triplet Sum to Zero (medium).py
@@ -24,6 +24,7 @@ def search_pair(arr, target_sum, left, triplets):
     right = len(arr) - 1
     while left < right:
         current_sum = arr[left] + arr[right]
+
         if current_sum == target_sum:  # found the triplet
             triplets.append([-target_sum, arr[left], arr[right]])
             left += 1
@@ -32,8 +33,10 @@ def search_pair(arr, target_sum, left, triplets):
                 left += 1  # skip same element to avoid duplicate triplets
             while left < right and arr[right] == arr[right + 1]:
                 right -= 1  # skip same element to avoid duplicate triplets
+
         elif target_sum > current_sum:
             left += 1  # we need a pair with a bigger sum
+
         else:
             right -= 1  # we need a pair with a smaller sum
 
