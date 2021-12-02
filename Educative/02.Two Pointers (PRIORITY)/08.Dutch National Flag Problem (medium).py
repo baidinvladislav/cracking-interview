983c4d5ef2dc44862a6601e027c2dbd52eed9a67
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-02 17:44:33 +0300

    Repeated this problem

--- Educative/02.Two Pointers (PRIORITY)/08.Dutch National Flag Problem (medium).py
+++ Educative/02.Two Pointers (PRIORITY)/08.Dutch National Flag Problem (medium).py
@@ -13,6 +13,7 @@ def dutch_flag_sort(arr):
     # all elements from >= low < i are 1
     low, high = 0, len(arr) - 1
     i = 0
+
     while i <= high:
         if arr[i] == 0:
             arr[i], arr[low] = arr[low], arr[i]
