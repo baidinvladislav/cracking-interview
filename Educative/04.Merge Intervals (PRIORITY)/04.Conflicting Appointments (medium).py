db81e08938962b4a011b3909e7bf82d88123ff46
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-06 17:41:29 +0300

    Repeated this problem

--- Educative/04.Merge Intervals (PRIORITY)/04.Conflicting Appointments (medium).py
+++ Educative/04.Merge Intervals (PRIORITY)/04.Conflicting Appointments (medium).py
@@ -9,11 +9,6 @@ def can_attend_all_appointments(intervals):
     start, end = 0, 1
     for i in range(1, len(intervals)):
         if intervals[i][start] < intervals[i - 1][end]:
-            # please note the comparison above, it is "<" and not "<="
-            # while merging we needed "<=" comparison, as we will be merging the two
-            # intervals having condition "intervals[i][start] == intervals[i - 1][end]" but
-            # such intervals don't represent conflicting appointments as one starts right
-            # after the other
             return False
     return True
 
