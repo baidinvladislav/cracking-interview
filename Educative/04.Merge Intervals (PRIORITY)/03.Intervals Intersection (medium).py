3c3270839152947106a58d5cab2e82586c17762d
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-06 17:19:45 +0300

    Repeated this problem

--- Educative/04.Merge Intervals (PRIORITY)/03.Intervals Intersection (medium).py
+++ Educative/04.Merge Intervals (PRIORITY)/03.Intervals Intersection (medium).py
@@ -9,19 +9,24 @@ def merge(intervals_a, intervals_b):
     i, j, start, end = 0, 0, 0, 1
 
     while i < len(intervals_a) and j < len(intervals_b):
+        interval_from_a_start, interval_from_a_end = intervals_a[i][start], intervals_a[i][end]
+        interval_from_b_start, interval_from_b_end = intervals_b[j][start], intervals_b[j][end]
+
         # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
-        a_overlaps_b = intervals_b[j][start] <= intervals_a[i][start] <= intervals_b[j][end]
+        a_overlaps_b = interval_from_b_start <= interval_from_a_start <= interval_from_b_end
 
         # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
-        b_overlaps_a = intervals_a[i][start] <= intervals_b[j][start] <= intervals_a[i][end]
+        b_overlaps_a = interval_from_a_start <= interval_from_b_start <= interval_from_a_end
 
-        # store the the intersection part
+        # store the intersection part
         if a_overlaps_b or b_overlaps_a:
-            result.append([max(intervals_a[i][start], intervals_b[j][start]), min(
-                intervals_a[i][end], intervals_b[j][end])])
+            result.append([
+                max(interval_from_a_start, interval_from_b_start),
+                min(interval_from_a_end, interval_from_b_end)
+            ])
 
         # move next from the interval which is finishing first
-        if intervals_a[i][end] < intervals_b[j][end]:
+        if interval_from_a_end < interval_from_b_end:
             i += 1
         else:
             j += 1
