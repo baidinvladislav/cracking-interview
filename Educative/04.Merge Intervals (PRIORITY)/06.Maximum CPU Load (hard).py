6f42a7379273197572e450d13109e3fa50695e8c
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-06 18:14:18 +0300

    Too hard. Did not understand it

--- Educative/04.Merge Intervals (PRIORITY)/06.Maximum CPU Load (hard).py
+++ Educative/04.Merge Intervals (PRIORITY)/06.Maximum CPU Load (hard).py
@@ -7,7 +7,7 @@ Our goal is to find the maximum CPU load at any time if all the jobs are running
 from heapq import *
 
 
-class job:
+class Job:
     def __init__(self, start, end, cpu_load):
         self.start = start
         self.end = end
@@ -37,9 +37,9 @@ def find_max_cpu_load(jobs):
 
 
 def main():
-    print("Maximum CPU load at any time: " + str(find_max_cpu_load([job(1, 4, 3), job(2, 5, 4), job(7, 9, 6)])))
-    print("Maximum CPU load at any time: " + str(find_max_cpu_load([job(6, 7, 10), job(2, 4, 11), job(8, 12, 15)])))
-    print("Maximum CPU load at any time: " + str(find_max_cpu_load([job(1, 4, 2), job(2, 4, 1), job(3, 6, 5)])))
+    print("Maximum CPU load at any time: " + str(find_max_cpu_load([Job(1, 4, 3), Job(2, 5, 4), Job(7, 9, 6)])))
+    print("Maximum CPU load at any time: " + str(find_max_cpu_load([Job(6, 7, 10), Job(2, 4, 11), Job(8, 12, 15)])))
+    print("Maximum CPU load at any time: " + str(find_max_cpu_load([Job(1, 4, 2), Job(2, 4, 1), Job(3, 6, 5)])))
 
 
 main()
