dfceeae53aa0c61edb317128dd332e1d2fa49733
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-06 11:00:01 +0300

    Repeated this problem

--- Educative/03.Fast & Slow pointers (PRIORITY)/03.Happy Number (medium).py
+++ Educative/03.Fast & Slow pointers (PRIORITY)/03.Happy Number (medium).py
@@ -9,7 +9,7 @@ Instead, they will be stuck in a cycle of numbers which does not include ‘1’
 def find_happy_number(num):
     slow, fast = num, num
     while True:
-        slow = find_square_sum(slow)  # move one step
+        slow = find_square_sum(slow)
         fast = find_square_sum(find_square_sum(fast))  # move two steps
         if slow == fast:  # found the cycle
             break
