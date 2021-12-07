fe9cca86d19e22571504c780049e078a8fa788eb
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-07 15:03:08 +0300

    Resolved 04

---
+++ Educative/17.Kth Smallest Number (hard)/04.Using Min-Heap.py
@@ -0,0 +1,9 @@
+"""
+Given an unsorted array of numbers, find Kth smallest number in it.
+
+Please note that it is the Kth smallest number in the sorted order, not the Kth distinct element.
+"""
+
+
+def find_Kth_smallest_number(nums, k):
+    pass
