95f75c94f88c69f4cf6898c52174048f953f873e
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-07 15:33:58 +0300

    Refactored solution

--- Educative/03.Fast & Slow pointers (DURING)/01.LinkedList Cycle (easy).py
+++ Educative/03.Fast & Slow pointers (DURING)/01.LinkedList Cycle (easy).py
@@ -12,11 +12,14 @@ class Node:
 
 def has_cycle(head):
     slow, fast = head, head
-    while fast is not None and fast.next is not None:
-        fast = fast.next.next
+
+    while fast and fast.next:
         slow = slow.next
+        fast = fast.next.next
+
         if slow == fast:
-            return True  # found the cycle
+            return True
+
     return False
 
 
