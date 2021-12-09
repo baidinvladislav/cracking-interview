e98e6c4105c2f243457c5785d124f846e43a4e98
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-09 14:26:58 +0300

    Wrote own solution

--- Educative/01.Sliding Window (DURING)/03.Smallest Subarray with a given sum (easy).py
+++ Educative/01.Sliding Window (DURING)/03.Smallest Subarray with a given sum (easy).py
@@ -6,25 +6,19 @@ Return 0 if no such subarray exists.
 """
 
 
-import math
-
-
 def smallest_subarray_with_given_sum(s, arr):
-    window_start, window_sum = 0, 0
-    min_length_sub = math.inf
-
+    window_start, window_sum, min_length = 0, 0, float('inf')
     for window_end in range(len(arr)):
         window_sum += arr[window_end]
         while window_sum >= s:
-            length_sub = window_end - window_start + 1
-            min_length_sub = min(min_length_sub, length_sub)
+            min_length = min(min_length, window_end - window_start + 1)
             window_sum -= arr[window_start]
             window_start += 1
 
-    if min_length_sub == math.inf:
+    if min_length == float('inf'):
         return 0
 
-    return min_length_sub
+    return min_length
 
 
 def main():
