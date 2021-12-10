4e4822c2849f8b510f493b12c8bad2da1e4ece52
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-10 17:12:15 +0300

    Got it

--- Educative/03.Fast & Slow pointers (DURING)/03.Happy Number (medium).py
+++ Educative/03.Fast & Slow pointers (DURING)/03.Happy Number (medium).py
@@ -10,10 +10,13 @@ def find_happy_number(num):
     slow, fast = num, num
     while True:
         slow = find_square_sum(slow)
-        fast = find_square_sum(find_square_sum(fast))  # move two steps
-        if slow == fast:  # found the cycle
+        # move two steps
+        fast = find_square_sum(find_square_sum(fast))
+        # found the cycle
+        if slow == fast:
             break
-    return slow == 1  # see if the cycle is stuck on the number '1'
+    # see if the cycle is stuck on the number '1'
+    return slow == 1
 
 
 def find_square_sum(num):
