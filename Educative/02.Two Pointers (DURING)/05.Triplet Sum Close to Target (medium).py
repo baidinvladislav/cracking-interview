4eba32ae17c665c2bd6c775e2fc99c60ab2d3076
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-11 11:45:03 +0300

    Refactored solution

--- Educative/02.Two Pointers (DURING)/05.Triplet Sum Close to Target (medium).py
+++ Educative/02.Two Pointers (DURING)/05.Triplet Sum Close to Target (medium).py
@@ -1,35 +1,34 @@
 """
-Given an array of unsorted numbers and a target number,
-find a triplet in the array whose sum is as close to the target number as possible,
-return the sum of the triplet. If there are more than one such triplet,
-return the sum of the triplet with the smallest sum.
+Given an array of unsorted numbers and a target number, find a triplet in the array whose sum is as close
+to the target number as possible, return the sum of the triplet.
+
+If there are more than one such triplet, return the sum of the triplet with the smallest sum.
 """
-
-
-import math
 
 
 def triplet_sum_close_to_target(arr, target_sum):
     arr.sort()
-    smallest_difference = math.inf
+    smallest_difference = float('inf')
+
     for i in range(len(arr) - 2):
         left = i + 1
         right = len(arr) - 1
+
         while left < right:
-            target_diff = target_sum - arr[i] - arr[left] - arr[right]
+            remainder = target_sum - arr[i] - arr[left] - arr[right]
 
-            if target_diff == 0:  # we've found a triplet with an exact sum
-                return target_sum - target_diff  # return sum of all the numbers
+            if remainder == 0:
+                return target_sum
 
-            # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-            if abs(target_diff) < abs(smallest_difference) or (
-                    abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
-                smallest_difference = target_diff  # save the closest and the biggest difference
+            if abs(remainder) < abs(smallest_difference) \
+                    or (abs(remainder) == abs(smallest_difference) and remainder > smallest_difference):
+                smallest_difference = remainder
 
-            if target_diff > 0:
-                left += 1  # we need a triplet with a bigger sum
-            else:
-                right -= 1  # we need a triplet with a smaller sum
+            if remainder > 0:
+                left += 1
+
+            elif remainder < 0:
+                right -= 1
 
     return target_sum - smallest_difference
 
