1c8ef4e832f2d0e84095df83d1f6886a36e37c28
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-14 16:11:46 +0300

    Refactored solution

--- Educative/04.Merge Intervals (DURING)/05.Minimum Meeting Rooms (hard).py
+++ Educative/04.Merge Intervals (DURING)/05.Minimum Meeting Rooms (hard).py
@@ -16,35 +16,28 @@ class Meeting:
         # min heap based on meeting.end
         return self.end < other.end
 
+    def __repr__(self):
+        return f'{self.start}:{self.end}'
+
 
 def min_meeting_rooms(meetings):
-    # sort the meetings by start time
     meetings.sort(key=lambda x: x.start)
+    min_room, min_heap = 0, []
 
-    minRooms = 0
-    minHeap = []
     for meeting in meetings:
-        # remove all the meetings that have ended
-        while len(minHeap) > 0 and meeting.start >= minHeap[0].end:
-            heappop(minHeap)
-        # add the current meeting into min_heap.
-        heappush(minHeap, meeting)
-        # all active meetings are in the min_heap, so we need rooms for all of them.
-        minRooms = max(minRooms, len(minHeap))
-    return minRooms
+        while len(min_heap) > 0 and meeting.start >= min_heap[0].end:
+            heappop(min_heap)
+
+        heappush(min_heap, meeting)
+        min_room = max(min_room, len(min_heap))
+    return min_room
 
 
 def main():
-    print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-        [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
-    print("Minimum meeting rooms required: " +
-          str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)])))
-    print("Minimum meeting rooms required: " +
-          str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)])))
-    print("Minimum meeting rooms required: " +
-          str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)])))
-    print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-        [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+    print(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)]))
+    print(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)]))
+    print(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)]))
+    print(min_meeting_rooms([Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]))
 
 
 main()
