34d3819ddb01666ed58d5eb18af4901ce6677849
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-15 16:11:07 +0300

    Refactored solution

--- Educative/01.Sliding Window (DURING)/07.Longest Substring with Same Letters after Replacement (hard).py
+++ Educative/01.Sliding Window (DURING)/07.Longest Substring with Same Letters after Replacement (hard).py
@@ -5,35 +5,27 @@ find the length of the longest substring having the same letters after replaceme
 
 
 # Time Complexity: O(N)
-# Space Complexity: O(1)
+# Space Complexity: O(1) - because there are only 26 symbols in alphabet
 def length_of_longest_substring(str1, k):
-    window_start, max_length, max_repeat_letter_count = 0, 0, 0
-    frequency_map = {}
+    window_start, max_length, max_repeat = 0, 0, 0
+    dict_index = {}
 
     for window_end in range(len(str1)):
-        # add character to hash-map
-        right_char = str1[window_end]
-        if right_char not in frequency_map:
-            frequency_map[right_char] = 0
-        frequency_map[right_char] += 1
-        # tracking the most repeatable character
-        max_repeat_letter_count = max(max_repeat_letter_count, frequency_map[right_char])
+        if str1[window_end] not in dict_index:
+            dict_index[str1[window_end]] = 0
+        dict_index[str1[window_end]] += 1
+        max_repeat = max(max_repeat, dict_index[str1[window_end]])
 
-        window_size = window_end - window_start + 1
-        diff = window_size - max_repeat_letter_count
-        # if window size includes unique symbols greater then number of available replacement
-        if diff > k:
-            # we'll shrink the window
-            left_char = str1[window_start]
-            frequency_map[left_char] -= 1
+        if k < window_end - window_start + 1 - max_repeat:
+            dict_index[str1[window_start]] -= 1
             window_start += 1
 
-        # update max length if it greater then it was
         max_length = max(max_length, window_end - window_start + 1)
     return max_length
 
 
 def main():
+    print(length_of_longest_substring("aaaaaaacc", 2))
     print(length_of_longest_substring("aabccbb", 2))
     print(length_of_longest_substring("abbcb", 1))
     print(length_of_longest_substring("abccde", 1))
