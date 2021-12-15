160bf836e11f99672aee7cafe67ef0f439393151
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-15 17:31:04 +0300

    Too hard. Can not understand 3th cycle

--- Educative/02.Two Pointers (DURING)/07.Subarrays with Product Less than a Target (medium) .py
+++ Educative/02.Two Pointers (DURING)/07.Subarrays with Product Less than a Target (medium) .py
@@ -7,22 +7,23 @@ find all of its contiguous subarrays whose product is less than the target numbe
 from collections import deque
 
 
+# Time Complexity: O(N**3)
+# Space Complexity: O(N)
 def find_subarrays(arr, target):
-    result = []
-    product = 1
-    left = 0
+    left, product, result = 0, 1, []
+
     for right in range(len(arr)):
         product *= arr[right]
+
         while product >= target and left < len(arr):
             product /= arr[left]
             left += 1
-        # since the product of all numbers from left to right is less than the target therefore,
-        # all subarrays from left to right will have a product less than the target too; to avoid
-        # duplicates, we will start with a subarray containing only arr[right] and then extend it
+
         temp_list = deque()
         for i in range(right, left - 1, -1):
             temp_list.appendleft(arr[i])
             result.append(list(temp_list))
+
     return result
 
 
