bd3ea981b42bd7043c5a0c35df84f6424a784505
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-16 17:52:26 +0300

    Added a docstring

--- Yandex LeetCode/49. Group Anagrams.py
+++ Yandex LeetCode/49. Group Anagrams.py
@@ -13,13 +13,15 @@ class Solution(object):
     def groupAnagrams(self, strs):
         """
         1. Sort the word alphabetically.
-        2. Insert sorted word into tuple data structure.
-        3. Insert the tuple in a dict data structure.
+        2. Insert sorted word into tuple data structure because tuple can be a key at a dict.
+        3. Insert a tuple into the dict data structure as a key, now we have a dictionary like this: {tuple: []}
         4. Insert a word to value array.
+        5. Return only values from dict without their keys.
         """
         char_map = collections.defaultdict(list)
         for word in strs:
-            key = tuple(sorted(word))
+            sorted_chars = sorted(word)
+            key = tuple(sorted_chars)
             char_map[key].append(word)
         return char_map.values()
 
