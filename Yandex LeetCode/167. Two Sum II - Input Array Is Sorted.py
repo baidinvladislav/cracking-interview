c4b196e7c9d2fff942910c7ff67c928518b70fc4
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-16 13:56:19 +0300

    Added docstring

--- Yandex LeetCode/167. Two Sum II - Input Array Is Sorted.py
+++ Yandex LeetCode/167. Two Sum II - Input Array Is Sorted.py
@@ -14,21 +14,20 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        low = 0
-        high = len(numbers) - 1
-
-        while low < high:
-            attempt = numbers[low] + numbers[high]
-
-            if attempt == target:
-                return [low + 1, high + 1]
-
-            elif attempt < target:
-                low += 1
-
-            elif attempt > target:
-                high -= 1
-
+        """
+        Two pointer approach.
+        1. Set left pointer to the beginning of the array and left pointer in the end.
+        2. If current sum less than `target` so increase left pointer to select greater integer.
+        3. If current sum greater than `target` so decrease right pointer to select less integer.
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
         return [-1, -1]
 
 
