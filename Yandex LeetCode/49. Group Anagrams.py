03b0eb81369ef0aae57d0327469b68026974d740
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-16 17:44:49 +0300

    Added a docstring

--- Yandex LeetCode/49. Group Anagrams.py
+++ Yandex LeetCode/49. Group Anagrams.py
@@ -11,10 +11,17 @@ import collections
 
 class Solution(object):
     def groupAnagrams(self, strs):
-        ans = collections.defaultdict(list)
+        """
+        1. Sort the word alphabetically.
+        2. Insert sorted word into tuple data structure.
+        3. Insert the tuple in a dict data structure.
+        4. Insert a word to value array.
+        """
+        char_map = collections.defaultdict(list)
         for word in strs:
-            ans[tuple(sorted(word))].append(word)
-        return ans.values()
+            key = tuple(sorted(word))
+            char_map[key].append(word)
+        return char_map.values()
 
 
 print(Solution().groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
