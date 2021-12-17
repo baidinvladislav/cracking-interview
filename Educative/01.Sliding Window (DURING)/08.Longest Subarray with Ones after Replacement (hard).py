4765ac222034e9599b99189d04b80270c2e66e4d
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-17 13:13:28 +0300

    Added a docstring

--- Educative/01.Sliding Window (DURING)/08.Longest Subarray with Ones after Replacement (hard).py
+++ Educative/01.Sliding Window (DURING)/08.Longest Subarray with Ones after Replacement (hard).py
@@ -7,18 +7,24 @@ find the length of the longest contiguous subarray having all 1s.
 # Time Complexity: O(N)
 # Space Complexity: O(1)
 def length_of_longest_substring(arr, k):
-    ones, window_start, max_length = 0, 0, 0
+    """
+    Sliding window approach.
+
+    1. Track number of '1'.
+    2. If there is more than `K` zero in the window we shrink the window.
+    3. During window shrink look at the beginning of the window if it is '1' then we decrease `ones` variable.
+    4. Update maximum length.
+    """
+    window_start, max_length, ones = 0, 0, 0
 
     for window_end in range(len(arr)):
         if arr[window_end] == 1:
             ones += 1
-
-        if window_end - window_start + 1 - ones > k:
+        if k < window_end - window_start + 1 - ones:
             if arr[window_start] == 1:
                 ones -= 1
             window_start += 1
-        max_length = max(window_end - window_start + 1, max_length)
-
+        max_length = max(max_length, window_end - window_start + 1)
     return max_length
 
 
