99ff665b52845540c6c7ed7b864a24ab4c54152a
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-17 15:54:45 +0300

    Added a docstring

--- Educative/02.Two Pointers (DURING)/09.Quadruple Sum to Target (medium).py
+++ Educative/02.Two Pointers (DURING)/09.Quadruple Sum to Target (medium).py
@@ -4,7 +4,21 @@ whose sum is equal to the target number.
 """
 
 
+# Time complexity: O(N**3)
+# Space complexity: O(N)
 def search_quadruplets(arr, target):
+    """
+    1. Algorithm will use four pointers.
+        first: first loop element
+        second: second loop element (first + 1)
+        left: pointer that run inside subarray[second:len(array) - 1]
+        right: pointer that run inside subarray[second:len(array) - 1: -1]
+    2. We sum four values arr[first] + arr[second] + arr[left] + arr[right].
+    3. If the current amount is "target", add these four values to the results array.
+    4. If the current quantity is less than the target, move the left pointer 1 to the right.
+    5. If the current sum is greater than the target value, we offset the right pointer by -1
+    to the beginning of the subarray [second:len(array) - 1].
+    """
     arr.sort()
     quadruplets = []
 
@@ -29,24 +43,29 @@ def search_pairs(arr, target_sum, first, second, quadruplets):
 
     while left < right:
         quad_sum = arr[first] + arr[second] + arr[left] + arr[right]
-        if quad_sum == target_sum:  # found the quadruplet
+        # found the quadruplet
+        if quad_sum == target_sum:
             quadruplets.append(
                 [arr[first], arr[second], arr[left], arr[right]]
             )
             left += 1
             right -= 1
 
+            # skip same element to avoid duplicate quadruplets
             while left < right and arr[left] == arr[left - 1]:
-                left += 1  # skip same element to avoid duplicate quadruplets
+                left += 1
 
+            # skip same element to avoid duplicate quadruplets
             while left < right and arr[right] == arr[right + 1]:
-                right -= 1  # skip same element to avoid duplicate quadruplets
+                right -= 1
 
+        # we need a pair with a bigger sum
         elif quad_sum < target_sum:
-            left += 1  # we need a pair with a bigger sum
+            left += 1
 
+        # we need a pair with a smaller sum
         else:
-            right -= 1  # we need a pair with a smaller sum
+            right -= 1
 
 
 def main():
