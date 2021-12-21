96ec408bde43674843c8f7a4d04b00d7df63d51b
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-21 17:18:22 +0300

    Added a docstring

--- Educative/02.Two Pointers (DURING)/11.Minimum Window Sort (medium).py
+++ Educative/02.Two Pointers (DURING)/11.Minimum Window Sort (medium).py
@@ -7,30 +7,33 @@ import math
 
 
 def shortest_window_sort(arr):
+    """
+    1. Find the first number out of sorting order from the beginning.
+    2. Find the first number out of sorting order from the end.
+    3. Find the maximum and minimum of the subarray.
+    4. Extend the subarray to include any number which is bigger than the minimum of the subarray.
+    5. Extend the subarray to include any number which is smaller than the maximum of the subarray.
+    """
     low, high = 0, len(arr) - 1
-    # find the first number out of sorting order from the beginning
     while low < len(arr) - 1 and arr[low] <= arr[low + 1]:
         low += 1
 
-    if low == len(arr) - 1:  # if the array is sorted
+    # if the array is sorted
+    if low == len(arr) - 1:
         return 0
 
-    # find the first number out of sorting order from the end
     while high > 0 and arr[high] >= arr[high - 1]:
         high -= 1
 
-    # find the maximum and minimum of the subarray
     subarray_max = -math.inf
     subarray_min = math.inf
     for k in range(low, high + 1):
         subarray_max = max(subarray_max, arr[k])
         subarray_min = min(subarray_min, arr[k])
 
-    # extend the subarray to include any number which is bigger than the minimum of the subarray
     while low > 0 and arr[low - 1] > subarray_min:
         low -= 1
 
-    # extend the subarray to include any number which is smaller than the maximum of the subarray
     while high < len(arr) - 1 and arr[high + 1] < subarray_max:
         high += 1
 
