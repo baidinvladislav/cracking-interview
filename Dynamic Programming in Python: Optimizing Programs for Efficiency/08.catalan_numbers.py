84119b82594f283024fee2fbb2bee118d47965f3
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-23 16:38:14 +0300

    Chapter #3

--- Dynamic Programming in Python: Optimizing Programs for Efficiency/08.catalan_numbers.py
+++ Dynamic Programming in Python: Optimizing Programs for Efficiency/08.catalan_numbers.py
@@ -1,15 +1,12 @@
 # Solution 1: Simple recursion
-def catalan(n):
+def recursion_catalan(n):
     if n == 0:  # base case; C(0) = 1
         return 1
     sum = 0
     # iterate from 1...n to evaluate: C(0)*C(n-1) + C(1)*C(n-2) ... + C(n-1)*C(0)
     for i in range(n):
-        sum += (catalan(i) * catalan(n - 1 - i))  # C(i)*C(n-1-i)
+        sum += (recursion_catalan(i) * recursion_catalan(n - 1 - i))  # C(i)*C(n-1-i)
     return sum
-
-
-print(catalan(4))
 
 
 # Solution 2: Top-down approach
@@ -26,25 +23,28 @@ def catalan_memo(n, memo):
     return memo[n]
 
 
-def catalan(n):
+def catalan_memo_helper(n):
     memo = {}
     return catalan_memo(n, memo)
 
 
-print(catalan(400))
-
-
 # Solution 3: Bottom-up dynamic programming
-def catalan(n):
+def bottom_up_catalan(n):
     table = [None] * (n + 1)  # tabulating
     table[0] = 1  # handling the base case
     for i in range(1, n + 1):  # iterating to fill up the tabulation table
         table[i] = 0  # initializing the i-th value to 0
-        # iterate from 0 to i; according to formula of catalan i.e.
+        # iterate from 0 to i; according to formula of recursion_catalan i.e.
         # C0*Ci + C1*Ci-1 + ... Ci*C0
         for j in range(i):
             table[i] += (table[j] * table[i - j - 1])  # C(j) * C(i-j-1)
     return table[n]
 
 
-print(catalan(1000))
+def main():
+    print(recursion_catalan(4))
+    print(catalan_memo_helper(400))
+    print(bottom_up_catalan(777))
+
+
+main()
