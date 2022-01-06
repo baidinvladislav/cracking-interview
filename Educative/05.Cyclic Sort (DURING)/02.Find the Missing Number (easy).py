249a8c583e785f3e0d0f33e44d0d4041fbea9be3
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-06 21:17:49 +0300

    Refactored solution

--- Educative/05.Cyclic Sort (DURING)/02.Find the Missing Number (easy).py
+++ Educative/05.Cyclic Sort (DURING)/02.Find the Missing Number (easy).py
@@ -5,25 +5,26 @@ Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, fin
 
 
 def find_missing_number(nums):
-    i, n = 0, len(nums)
-    while i < n:
-        j = nums[i]
-        if nums[i] < n and nums[i] != nums[j]:
-            nums[i], nums[j] = nums[j], nums[i]  # swap
-        else:
-            i += 1
+    pointer, x = 0, len(nums)
 
-    # find the first number missing from its index, that will be our required number
-    for i in range(n):
-        if nums[i] != i:
+    while pointer < x:
+        correct_idx = nums[pointer]
+        if nums[pointer] < x and nums[pointer] != nums[correct_idx]:
+            nums[pointer], nums[correct_idx] = nums[correct_idx], nums[pointer]
+        else:
+            pointer += 1
+
+    for i in range(x):
+        if i != nums[i]:
             return i
 
-    return n
+    return x
 
 
 def main():
     print(find_missing_number([4, 0, 3, 1]))
     print(find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+    print(find_missing_number([6, 5, 4, 3, 2, 0]))
 
 
 main()
