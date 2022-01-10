f4ce2592b3cfe28ee6b0e4f8d0061cbb77f5898a
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-10 14:38:11 +0300

    Refactored solution

--- Educative/05.Cyclic Sort (DURING)/06.Find the Corrupt Pair (easy).py
+++ Educative/05.Cyclic Sort (DURING)/06.Find the Corrupt Pair (easy).py
@@ -9,9 +9,9 @@ Find both these numbers.
 def find_corrupt_numbers(nums):
     i = 0
     while i < len(nums):
-        j = nums[i] - 1
-        if nums[i] != nums[j]:
-            nums[i], nums[j] = nums[j], nums[i]  # swap
+        correct_idx = nums[i] - 1
+        if nums[i] != nums[correct_idx]:
+            nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
         else:
             i += 1
 
@@ -23,8 +23,8 @@ def find_corrupt_numbers(nums):
 
 
 def main():
-    print(find_corrupt_numbers([3, 1, 2, 5, 2]))
-    print(find_corrupt_numbers([3, 1, 2, 3, 6, 4]))
+    print(find_corrupt_numbers([3, 1, 2, 5, 2]))  # [2, 4]
+    print(find_corrupt_numbers([3, 1, 2, 3, 6, 4]))  # [3, 5]
 
 
 main()
