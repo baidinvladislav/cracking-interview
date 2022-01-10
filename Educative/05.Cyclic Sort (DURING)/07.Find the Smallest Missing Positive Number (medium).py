545a63d0f01a6db779853205326ff1cf8c064276
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-10 15:07:08 +0300

    Refactored solution

--- Educative/05.Cyclic Sort (DURING)/07.Find the Smallest Missing Positive Number (medium).py
+++ Educative/05.Cyclic Sort (DURING)/07.Find the Smallest Missing Positive Number (medium).py
@@ -6,9 +6,9 @@ Given an unsorted array containing numbers, find the smallest missing positive n
 def find_first_smallest_missing_positive(nums):
     i, n = 0, len(nums)
     while i < n:
-        j = nums[i] - 1
-        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
-            nums[i], nums[j] = nums[j], nums[i]  # swap
+        correct_idx = nums[i] - 1
+        if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
+            nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
         else:
             i += 1
 
@@ -16,7 +16,7 @@ def find_first_smallest_missing_positive(nums):
         if nums[i] != i + 1:
             return i + 1
 
-    return len(nums) + 1
+    return [-1, -1]
 
 
 def main():
