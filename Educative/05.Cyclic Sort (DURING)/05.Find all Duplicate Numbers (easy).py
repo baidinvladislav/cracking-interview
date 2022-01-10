ce49292fc2c3106474b25bf627e4222b3ab57efe
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-10 14:08:57 +0300

    Refactored solution

--- Educative/05.Cyclic Sort (DURING)/05.Find all Duplicate Numbers (easy).py
+++ Educative/05.Cyclic Sort (DURING)/05.Find all Duplicate Numbers (easy).py
@@ -7,23 +7,24 @@ The array has some numbers appearing twice, find all these duplicate numbers wit
 def find_all_duplicates(nums):
     i = 0
     while i < len(nums):
-        j = nums[i] - 1
-        if nums[i] != nums[j]:
-            nums[i], nums[j] = nums[j], nums[i]  # swap
+        correct_idx = nums[i] - 1
+        if nums[i] != nums[correct_idx]:
+            nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
         else:
             i += 1
 
-    duplicateNumbers = []
+    duplicates = []
     for i in range(len(nums)):
         if nums[i] != i + 1:
-            duplicateNumbers.append(nums[i])
+            duplicates.append(nums[i])
 
-    return duplicateNumbers
+    return duplicates
 
 
 def main():
     print(find_all_duplicates([3, 4, 4, 5, 5]))
     print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
+    print(find_all_duplicates([1, 2, 3, 3, 2, 5, 6]))
 
 
 main()
