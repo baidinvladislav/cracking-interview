5ef1dd2c3e20e4609da39ec9868c9cb2adaf138f
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-10 16:00:30 +0300

    Refactored solution

--- Educative/05.Cyclic Sort (DURING)/08.Find the First K Missing Positive Numbers (hard).py
+++ Educative/05.Cyclic Sort (DURING)/08.Find the First K Missing Positive Numbers (hard).py
@@ -9,29 +9,29 @@ def find_first_k_missing_positive(nums, k):
     i = 0
     while i < len(nums):
         j = nums[i] - 1
-        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
-            nums[i], nums[j] = nums[j], nums[i]  # swap
+        if 0 < nums[i] <= n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
         else:
             i += 1
 
-    missingNumbers = []
-    extraNumbers = set()
+    missing_numbers = []
+    extra_numbers = set()
     for i in range(n):
-        if len(missingNumbers) < k:
+        if len(missing_numbers) < k:
             if nums[i] != i + 1:
-                missingNumbers.append(i + 1)
-                extraNumbers.add(nums[i])
+                missing_numbers.append(i + 1)
+                extra_numbers.add(nums[i])
 
     # add the remaining missing numbers
     i = 1
-    while len(missingNumbers) < k:
-        candidateNumber = i + n
+    while len(missing_numbers) < k:
+        candidate_number = i + n
         # ignore if the array contains the candidate number
-        if candidateNumber not in extraNumbers:
-            missingNumbers.append(candidateNumber)
+        if candidate_number not in extra_numbers:
+            missing_numbers.append(candidate_number)
         i += 1
 
-    return missingNumbers
+    return missing_numbers
 
 
 def main():
