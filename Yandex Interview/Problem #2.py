818805b8e2f47d20468c9739fccd24f7b4ef80ab
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-16 17:53:00 +0300

    Resolved #2

--- Yandex Interview/Problem #2.py
+++ Yandex Interview/Problem #2.py
@@ -5,3 +5,25 @@
 Пояснения: Если символ встречается 1 раз, он остается без изменений;
 Если символ повторяется более 1 раза, к нему добавляется количество повторений.\
 """
+
+
+class Solution:
+    def f(self, s):
+        if not s:
+            return 'на вход пришла невалидная строка'
+
+        s += '#'
+        result = ''
+        p1, p2 = 0, 1
+
+        while p2 != len(s):
+            if s[p1] == s[p2]:
+                p2 += 1
+            else:
+                result += f'{p2 - p1}{s[p1]}' if p2 - p1 != 1 else f'{s[p1]}'
+                p1 = p2
+        return result
+
+
+print(Solution().f(s='AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
+print(Solution().f(s=''))
