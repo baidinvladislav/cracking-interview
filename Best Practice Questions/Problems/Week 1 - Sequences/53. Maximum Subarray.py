75d95dd951bf1b429ee36d5ef05a2af8029e63b1
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-18 17:14:11 +0300

    18.02.22

--- Best Practice Questions/Problems/Week 1 - Sequences/53. Maximum Subarray.py
+++ Best Practice Questions/Problems/Week 1 - Sequences/53. Maximum Subarray.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(1, len(nums)):
             current_subarray = max(nums[i], current_subarray + nums[i])
             max_subarray = max(current_subarray, max_subarray)
-
         return max_subarray
 
 
