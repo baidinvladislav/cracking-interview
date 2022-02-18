57db59453551d3a3275ed6990280346d51c9fe91
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-18 17:55:23 +0300

    18.02.22

--- Best Practice Questions/Problems/Week 1 - Sequences/56. Merge Intervals.py
+++ Best Practice Questions/Problems/Week 1 - Sequences/56. Merge Intervals.py
@@ -6,21 +6,17 @@ class Solution:
         if len(intervals) < 2:
             return intervals
 
-        intervals.sort(key=lambda x: x[0])
         merged_intervals = []
+        intervals.sort(key=lambda x: x[0])
 
-        start = intervals[0][0]
-        end = intervals[0][1]
-
+        start, end = intervals[0][0], intervals[0][1]
         for i in range(1, len(intervals)):
-            interval = intervals[i]
-            if end >= interval[0]:
-                end = max(interval[1], end)
+            if intervals[i][0] <= end:
+                end = max(end, intervals[i][1])
             else:
                 merged_intervals.append([start, end])
-                start = interval[0]
-                end = interval[1]
-
+                start = intervals[i][0]
+                end = intervals[i][1]
         merged_intervals.append([start, end])
         return merged_intervals
 
