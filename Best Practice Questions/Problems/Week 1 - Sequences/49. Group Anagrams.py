51bddca6c771ce826ebb41696c8a6a585cbfcd10
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-21 18:53:43 +0300

    21.02.22

--- Best Practice Questions/Problems/Week 1 - Sequences/49. Group Anagrams.py
+++ Best Practice Questions/Problems/Week 1 - Sequences/49. Group Anagrams.py
@@ -4,13 +4,11 @@ from collections import defaultdict
 
 class Solution(object):
     def groupAnagrams(self, strs):
-        if not strs:
-            return [[""]]
-
         hash_map = defaultdict(list)
         for word in strs:
-            hash_map[tuple(sorted(word))].append(word)
-        return list(hash_map.values())
+            sorted_word = tuple(sorted(word))
+            hash_map[sorted_word].append(word)
+        return hash_map.values()
 
 
 class TestMaxProduct(unittest.TestCase):
