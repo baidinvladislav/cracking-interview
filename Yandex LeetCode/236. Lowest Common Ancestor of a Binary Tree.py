e171509b48c1454cda57919083a06dcc02684a3f
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-13 17:23:38 +0300

    13.03.22 Recursion

---
+++ Yandex LeetCode/236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return str(self.val)
+
+
+class Solution:
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
+
+    def lowestCommonAncestor(self, root, p, q):
+        def recurse_tree(current_node):
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+            # If the current node is one of p or q
+            mid = current_node.val == p.val or current_node.val == q.val
+
+            # If any two of the three flags left, right or mid become True.
+            if mid + left + right >= 2:
+                self.ans = current_node
+
+            # Return True if either of the three bool values is True.
+            return mid or left or right
+
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
+
+
+root = TreeNode(3)
+
+root.left = TreeNode(5)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+
+root.right = TreeNode(1)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+
+
+p, q = TreeNode(5), TreeNode(1)
+print(Solution().lowestCommonAncestor(root=root, p=p, q=q))
