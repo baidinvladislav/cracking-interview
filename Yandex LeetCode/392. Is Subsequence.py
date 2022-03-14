46eebf0bc510df9f8964ac5fd814a16d530d2e34
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-14 13:23:22 +0300

    14.03.22

--- Yandex LeetCode/392. Is Subsequence.py
+++ Yandex LeetCode/392. Is Subsequence.py
@@ -3,22 +3,16 @@ class Solution:
     # Time Complexity: O(T)
     # Space Complexity: O(T)
     def isSubsequence(self, s: str, t: str) -> bool:
-        source_length, target_length = len(s), len(t)
+        if not s:
+            return True
+        if not t:
+            return False
 
-        def rec_isSubsequence(left_index, right_index):
-            # base cases
-            if left_index == source_length:
-                return True
-            if right_index == target_length:
-                return False
+        if s[0] == t[0]:
+            s = s[1:]
+        t = t[1:]
 
-            # consume both strings or just the target string
-            if s[left_index] == t[right_index]:
-                left_index += 1
-            right_index += 1
-            return rec_isSubsequence(left_index, right_index)
-
-        return rec_isSubsequence(0, 0)
+        return self.isSubsequence(s, t)
 
 
 def main():
