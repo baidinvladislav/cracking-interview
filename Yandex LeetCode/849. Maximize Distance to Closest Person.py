02b2c9f2267899f6fd0578a749d36133b2c3d2c1
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-17 13:50:57 +0300

    17.03.22

---
+++ Yandex LeetCode/849. Maximize Distance to Closest Person.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        n = len(seats)
+        empty, result, idx1, idx2 = 0, 0, -1, -1
+
+        for i in range(n):
+            if seats[i] == 1:
+                empty = 0
+
+                if idx1 == -1:
+                    idx1 = i
+                idx2 = i
+
+            else:
+                empty += 1
+                result = max(result, (empty + 1) // 2)
+
+        result = max(result, idx1, n - 1 - idx2)
+        return result
+
+
+def main():
+    print(Solution().maxDistToClosest(seats=[1, 0, 0, 0, 1, 0, 1]))
+    print(Solution().maxDistToClosest(seats=[1, 0, 0, 0]))
+    print(Solution().maxDistToClosest(seats=[0, 1]))
+
+
+main()
