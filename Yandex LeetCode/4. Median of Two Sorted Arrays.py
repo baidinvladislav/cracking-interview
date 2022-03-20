05e42016b0d9813820f0001ee8e74cdeda07b9b1
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-20 17:35:59 +0300

    20.03.22

--- Yandex LeetCode/4. Median of Two Sorted Arrays.py
+++ Yandex LeetCode/4. Median of Two Sorted Arrays.py
@@ -1,80 +1,31 @@
 class Solution:
-    def find(self, array1, first_idx1, last_idx1, array2, first_idx2, last_idx2, length_half):
-        if last_idx1 - first_idx1 < 0:
-            return array2[length_half + first_idx2]
-
-        if last_idx2 - first_idx2 < 0:
-            return array1[length_half + first_idx1]
-
-        if length_half < 1:
-            return min(array1[length_half + first_idx1], array2[length_half + first_idx2])
-
-        ia, ib = (first_idx1 + last_idx1) // 2, (first_idx2 + last_idx2) // 2
-        ma, mb = array1[ia], array2[ib]
-        if (ia - first_idx1) + (ib - first_idx2) < length_half:
-            if ma > mb:
-                return self.find(
-                    array1, first_idx1, last_idx1, array2, ib + 1, last_idx2, length_half - (ib - first_idx2) - 1
-                )
-            else:
-                return self.find(
-                    array1, ia + 1, last_idx1, array2, first_idx2, last_idx2, length_half - (ia - first_idx1) - 1
-                )
-        else:
-            if ma > mb:
-                return self.find(
-                    array1, first_idx1, ia - 1, array2, first_idx2, last_idx2, length_half
-                )
-            else:
-                return self.find(
-                    array1, first_idx1, last_idx1, array2, first_idx2, ib - 1, length_half
-                )
-
     def findMedianSortedArrays(self, nums1, nums2):
-        length = len(nums1) + len(nums2)
-        last_idx1 = len(nums1) - 1
-        last_idx2 = len(nums2) - 1
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        # odd
-        if length % 2 == 1:
-            return self.find(
-                array1=nums1,
-                first_idx1=0,
-                last_idx1=last_idx1,
+        m, n = len(nums1), len(nums2)
 
-                array2=nums2,
-                first_idx2=0,
-                last_idx2=last_idx2,
+        left_size = (m + n + 1) // 2
+        start, end = 0, m
+        is_even = ((m + n) % 2) == 0
+        while start <= end:
+            a_part = (start + end) // 2
+            b_part = left_size - a_part
 
-                length_half=length // 2
-            )
-        # even
-        else:
-            a = self.find(
-                array1=nums1,
-                first_idx1=0,
-                last_idx1=last_idx1,
+            aleftmax = float("-inf") if a_part == 0 else nums1[a_part - 1]
+            arightmin = float("inf") if a_part == m else nums1[a_part]
 
-                array2=nums2,
-                first_idx2=0,
-                last_idx2=last_idx2,
+            bleftmax = float("-inf") if b_part == 0 else nums2[b_part - 1]
+            brightmin = float("inf") if b_part == n else nums2[b_part]
 
-                length_half=length // 2
-            )
+            if aleftmax <= brightmin and bleftmax <= arightmin:
+                if not is_even:
+                    return max(aleftmax, bleftmax)
+                else:
+                    return (max(aleftmax, bleftmax) + min(arightmin, brightmin)) / 2
 
-            b = self.find(
-                array1=nums1,
-                first_idx1=0,
-                last_idx1=last_idx1,
+            elif aleftmax > brightmin:
+                end = a_part - 1
 
-                array2=nums2,
-                first_idx2=0,
-                last_idx2=last_idx2,
-
-                length_half=length // 2 - 1
-            )
-
-            return a + b / 2.0
-
-
-print(Solution().findMedianSortedArrays(nums1=[1, 2], nums2=[3, 4]))
+            elif bleftmax > arightmin:
+                start = a_part + 1
