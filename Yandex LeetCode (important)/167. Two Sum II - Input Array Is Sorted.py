28432ca09223b94fcde785cd908b9358c060c1f9
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-21 15:20:13 +0300

    #167

--- Yandex LeetCode (important)/167. Two Sum II - Input Array Is Sorted.py
+++ Yandex LeetCode (important)/167. Two Sum II - Input Array Is Sorted.py
@@ -1,15 +1,3 @@
-"""
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
-find two numbers such that they add up to a specific target number.
-Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-
-Return the indices of the two numbers, index1 and index2,
-added by one as an integer array [index1, index2] of length 2.
-
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
-"""
-
-
 from typing import List
 
 
@@ -17,12 +5,6 @@ class Solution:
     # Time complexity : O(n)
     # Space complexity : O(1)
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        """
-        Two pointer approach.
-        1. Set left pointer to the beginning of the array and left pointer in the end.
-        2. If current sum less than `target` so increase left pointer to select greater integer.
-        3. If current sum greater than `target` so decrease right pointer to select less integer.
-        """
         left, right = 0, len(numbers) - 1
         while left < right:
             if numbers[left] + numbers[right] == target:
