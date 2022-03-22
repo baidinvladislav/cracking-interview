72f73aa1f13a1a4d4f9c5cef0a0b9ccf881a3b9b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-22 13:25:05 +0300

    Update: 283.Move Zeroes

--- Yandex LeetCode (important)/problems/283. Move Zeroes.py
+++ Yandex LeetCode (important)/problems/283. Move Zeroes.py
@@ -7,6 +7,22 @@ Note that you must do this in-place without making a copy of the array.
 
 
 class Solution:
+    # Approach #2 (Space Optimal, Operation Sub-Optimal)
+    # Space Complexity: O(1)
+    # Time Complexity: O(n)
+    def moveZeroes(self, nums):
+        lastNonZeroFoundAt = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[lastNonZeroFoundAt] = nums[i]
+                lastNonZeroFoundAt += 1
+
+        for i in range(lastNonZeroFoundAt, len(nums)):
+            nums[i] = 0
+
+    # Approach #3 (Optimal)
+    # Space Complexity: O(1)
+    # Time Complexity: O(n)
     def moveZeroes(self, nums):
         """
         1. if the number is 0 then we only increment the pointer named "second".
