77bb5f344512b9acb6da5e0fa1ba9ed1fb47dff4
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-23 12:37:01 +0300

    Update: 21. Merge Two Sorted Lists

--- Yandex LeetCode (important)/problems/21. Merge Two Sorted Lists.py
+++ Yandex LeetCode (important)/problems/21. Merge Two Sorted Lists.py
@@ -24,7 +24,6 @@ class Solution:
         if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
-
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
