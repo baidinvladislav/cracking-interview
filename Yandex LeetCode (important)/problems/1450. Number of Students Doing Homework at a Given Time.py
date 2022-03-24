2af92e7d0bb47d7a68c1ce2b93a7792e1f7dd5b3
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-24 16:49:41 +0300

    Update: 1450. Number of Students Doing Homework at a Given Time

--- Yandex LeetCode (important)/problems/1450. Number of Students Doing Homework at a Given Time.py
+++ Yandex LeetCode (important)/problems/1450. Number of Students Doing Homework at a Given Time.py
@@ -3,11 +3,11 @@ from typing import List
 
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        pupils = 0
+        students = 0
         for i in range(len(endTime)):
             if startTime[i] <= queryTime <= endTime[i]:
-                pupils += 1
-        return pupils
+                students += 1
+        return students
 
 
 print(Solution().busyStudent(startTime=[1, 2, 3], endTime=[3, 2, 7], queryTime=4))
