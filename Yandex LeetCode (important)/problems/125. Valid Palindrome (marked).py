9b33154262b491f330eaf54d36adc29cad6c36d4
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-25 13:59:52 +0300

    Update: 125. Valid Palindrome

--- Yandex LeetCode (important)/problems/125. Valid Palindrome (marked).py
+++ Yandex LeetCode (important)/problems/125. Valid Palindrome (marked).py
@@ -10,11 +10,30 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n)
+    # def isPalindrome(self, s: str) -> bool:
+    #     filtered_symbols = filter(lambda ch: ch.isalnum(), s)
+    #     low_case = map(lambda ch: ch.lower(), filtered_symbols)
+    #     result_list = list(low_case)
+    #     return result_list == result_list[::-1]
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
     def isPalindrome(self, s: str) -> bool:
-        filtered_symbols = filter(lambda ch: ch.isalnum(), s)
-        low_case = map(lambda ch: ch.lower(), filtered_symbols)
-        result_list = list(low_case)
-        return result_list == result_list[::-1]
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
 
 
 print(Solution().isPalindrome(s="race a car"))
