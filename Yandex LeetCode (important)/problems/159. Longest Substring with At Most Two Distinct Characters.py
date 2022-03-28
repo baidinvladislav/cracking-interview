fc53719ed7572c81a27d283c7d7d8dc991cd865b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-28 16:33:58 +0300

    Update: 159. Longest Substring with At Most Two Distinct Characters

--- Yandex LeetCode (important)/problems/159. Longest Substring with At Most Two Distinct Characters.py
+++ Yandex LeetCode (important)/problems/159. Longest Substring with At Most Two Distinct Characters.py
@@ -4,39 +4,26 @@ that contains at most two distinct characters.
 """
 
 
-from collections import defaultdict
-
-
 class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def lengthOfLongestSubstringTwoDistinct(self, s: 'str') -> 'int':
-        n = len(s)
-        if n < 3:
-            return n
+        window_start = 0
+        max_length = float('-inf')
+        hash_map = {}
 
-        # sliding window left and right pointers
-        left, right = 0, 0
-        # hashmap character -> its rightmost position
-        # in the sliding window
-        hashmap = defaultdict()
+        for window_end in range(len(s)):
+            if s[window_end] not in hash_map:
+                hash_map[s[window_end]] = 0
+            hash_map[s[window_end]] += 1
 
-        max_len = 2
-
-        while right < n:
-            # when the slidewindow contains less than 3 characters
-            hashmap[s[right]] = right
-            right += 1
-
-            # slidewindow contains 3 characters
-            if len(hashmap) == 3:
-                # delete the leftmost character
-                del_idx = min(hashmap.values())
-                del hashmap[s[del_idx]]
-                # move left pointer of the slidewindow
-                left = del_idx + 1
-
-            max_len = max(max_len, right - left)
-
-        return max_len
+            while len(hash_map) > 2:
+                hash_map[s[window_start]] -= 1
+                if hash_map[s[window_start]] == 0:
+                    del hash_map[s[window_start]]
+                window_start += 1
+            max_length = max(max_length, window_end - window_start + 1)
+        return max_length
 
 
 print(Solution().lengthOfLongestSubstringTwoDistinct(s="ccaabbb"))
