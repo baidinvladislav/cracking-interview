40764622928aaf52654c44aafea0d56adee9ad4f
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-04-01 14:14:54 +0300

    Update: 356. Line Reflection

--- Yandex LeetCode (important)/problems/356. Line Reflection.py
+++ Yandex LeetCode (important)/problems/356. Line Reflection.py
@@ -3,16 +3,24 @@ from typing import List
 
 class Solution:
     def isReflected(self, points: List[List[int]]) -> bool:
+        # remove duplicates
         points = set(map(tuple, points))
+
+        # find max and min on x
         minx, _ = min(points, key=lambda x: x[0])
         maxx, _ = max(points, key=lambda x: x[0])
+
+        # calculate a line between min x and max x
         rx = (minx + maxx) / 2
 
         result = []
         for x, y in points:
+            # create a mirror point
             point = (2 * rx - x, y)
+            # check that a mirror point in the points
             result.append(point in points)
 
+        # return True if we have all mirrors points otherwise False
         return all(result)
 
 
