2575f0f45d5e3bfc9ff242e28f7a3d17c88c7951
baydinvladislav <baydinvladislav@yandex.ru>
2022-06-14 22:39:37 +0300

    RESOLVED: 14.06.22

---
+++ LeetCode SQL I/176. Second Highest Salary.sql
@@ -0,0 +1,7 @@
+-- Find max where value is not equals to max salary (subquery)
+-- https://leetcode.com/problems/second-highest-salary/solution/
+
+
+SELECT max(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary NOT IN (SELECT max(salary) FROM Employee);
