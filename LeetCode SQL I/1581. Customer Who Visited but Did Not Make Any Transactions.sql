34d2e03b8176c209d6da76a9e77e8c9aac5292db
baydinvladislav <baydinvladislav@yandex.ru>
2022-06-15 20:01:20 +0300

    Resolved

---
+++ LeetCode SQL I/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,9 @@
+# take those customers and his times without transactions
+# who don't represent into Transaction table (subquery)
+# then we group by every customer
+
+
+SELECT customer_id, COUNT(customer_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY customer_id;
