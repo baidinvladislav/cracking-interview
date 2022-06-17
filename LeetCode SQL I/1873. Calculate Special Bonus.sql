f06f278c8735e5e18c4f76b348cbf2c737abe1be
baydinvladislav <baydinvladislav@yandex.ru>
2022-06-18 00:30:43 +0300

    Day #2

---
+++ LeetCode SQL I/1873. Calculate Special Bonus.sql
@@ -0,0 +1,13 @@
+# create dynamic field with CASE
+#
+
+
+SELECT
+employee_id,
+CASE
+    WHEN employee_id % 2 = 0 OR name LIKE 'M%' THEN 0
+    ELSE salary
+END
+AS bonus
+FROM Employees
+ORDER BY employee_id;
