ec55b0b6a7ab995689a8817ad97a1eef352979f6
baydinvladislav <baydinvladislav@yandex.ru>
2022-06-30 16:38:46 +0300

    Clear implementation

--- Sortings/merge_sort.py
+++ Sortings/merge_sort.py
@@ -1,35 +1,39 @@
 import random
 
 
-def merge_two_list(list_a, list_b):
+def merge_sort(list_to_sort):
+    # Base case: lists with fewer than 2 elements are sorted
+    if len(list_to_sort) < 2:
+        return list_to_sort
+
+    # Step 1: divide the list in half
+    # We use integer division, so we'll never get a "half index"
+    mid_index = len(list_to_sort) // 2
+    left  = list_to_sort[:mid_index]
+    right = list_to_sort[mid_index:]
+
+    # Step 2: sort each half
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+
+    # Step 3: merge the sorted halves
     sorted_list = []
-    i = j = 0
+    current_index_left = 0
+    current_index_right = 0
 
-    while i < len(list_a) and j < len(list_b):
-        if list_a[i] < list_b[j]:
-            sorted_list.append(list_a[i])
-            i += 1
+    # sortedLeft's first element comes next
+    # if it's less than sortedRight's first
+    # element or if sortedRight is exhausted
+    while len(sorted_list) < len(left) + len(right):
+        if ((current_index_left < len(left)) and
+                (current_index_right == len(right) or
+                 sorted_left[current_index_left] < sorted_right[current_index_right])):
+            sorted_list.append(sorted_left[current_index_left])
+            current_index_left += 1
         else:
-            sorted_list.append(list_b[j])
-            j += 1
-
-    if i < len(list_a):
-        sorted_list += list_a[i:]
-
-    if j < len(list_b):
-        sorted_list += list_b[j:]
-
+            sorted_list.append(sorted_right[current_index_right])
+            current_index_right += 1
     return sorted_list
-
-
-def merge_sort(array):
-    if len(array) == 1:
-        return array
-
-    middle = len(array)//2
-    left = merge_sort(array[:middle])
-    right = merge_sort(array[middle:])
-    return merge_two_list(left, right)
 
 
 if __name__ == '__main__':
