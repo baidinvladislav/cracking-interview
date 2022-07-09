b809d322271df12cdc8c64b4d57b8832e5dc2326
baydinvladislav <baydinvladislav@yandex.ru>
2022-07-09 22:32:02 +0300

    [created]: resolved

---
+++ Interview Cake Course/1. Array and string manipulation/2.Reverse String in Place.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+def reverse(list_of_chars):
+    start, end = 0, len(list_of_chars) - 1
+    while start < end:
+        list_of_chars[start], list_of_chars[end] = list_of_chars[end], list_of_chars[start]
+
+        start += 1
+        end -= 1
+
+
+class Test(unittest.TestCase):
+
+    def test_empty_string(self):
+        list_of_chars = []
+        reverse(list_of_chars)
+        expected = []
+        self.assertEqual(list_of_chars, expected)
+
+    def test_single_character_string(self):
+        list_of_chars = ['A']
+        reverse(list_of_chars)
+        expected = ['A']
+        self.assertEqual(list_of_chars, expected)
+
+    def test_longer_string(self):
+        list_of_chars = ['A', 'B', 'C', 'D', 'E']
+        reverse(list_of_chars)
+        expected = ['E', 'D', 'C', 'B', 'A']
+        self.assertEqual(list_of_chars, expected)
+
+
+unittest.main(verbosity=2)
