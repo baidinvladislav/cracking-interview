1727074c5c8eaeac2dbb752545342bc30d8a3549
vbaydin <vbaydin@redmadrobot.com>
2022-07-12 09:31:15 +0300

    [created]: resolved

---
+++ InterviewCake/2. Hashing and hash tables/1. Inflight Entertainment.py
@@ -0,0 +1,56 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+    films = set()
+
+    for movie in movie_lengths:
+        free_time = flight_length - movie
+        if free_time in films:
+            return True
+
+        films.add(movie)
+
+    return False
+
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_multiple_movies_shorter_than_flight(self):
+        result = can_two_movies_fill_flight([5, 6, 7, 8], 9)
+        self.assertFalse(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
