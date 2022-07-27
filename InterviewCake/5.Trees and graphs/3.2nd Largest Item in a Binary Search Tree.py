5469eaeaa86e5d1399ee565a0256eaae5b96c010
baydinvladislav <baydinvladislav@yandex.ru>
2022-07-27 17:25:02 +0300

    [updated]: added their solution

--- InterviewCake/5.Trees and graphs/3.2nd Largest Item in a Binary Search Tree.py
+++ InterviewCake/5.Trees and graphs/3.2nd Largest Item in a Binary Search Tree.py
@@ -1,6 +1,9 @@
 import unittest
 
 
+# my own solution
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def find_second_largest(root_node):
     def dfs(node):
         if not node:
@@ -14,6 +17,39 @@ def find_second_largest(root_node):
     dfs(root_node)
 
     return values[-2]
+
+
+# their solution
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def find_largest(root_node):
+    current = root_node
+    while current:
+        if not current.right:
+            return current.value
+        current = current.right
+
+
+def find_second_largest(root_node):
+    if (root_node is None or
+            (root_node.left is None and root_node.right is None)):
+        raise ValueError('Tree must have at least 2 nodes')
+
+    current = root_node
+    while current:
+        # Case: current is largest and has a left subtree
+        # 2nd largest is the largest in that subtree
+        if current.left and not current.right:
+            return find_largest(current.left)
+
+        # Case: current is parent of largest, and largest has no children,
+        # so current is 2nd largest
+        if (current.right and
+                not current.right.left and
+                not current.right.right):
+            return current.value
+
+        current = current.right
 
 
 class Test(unittest.TestCase):
