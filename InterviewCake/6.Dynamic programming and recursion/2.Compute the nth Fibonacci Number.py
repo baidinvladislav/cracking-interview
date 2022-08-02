0c6b46207574bdb99b7d4089204ced6ca5560b10
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-02 09:51:15 +0300

    [updated]: added my own

---
+++ InterviewCake/6.Dynamic programming and recursion/2.Compute the nth Fibonacci Number.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+# my own solution
+def fib(n):
+    if not n:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fib(n - 2) + fib(n - 1)
+
+
+class Test(unittest.TestCase):
+
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
