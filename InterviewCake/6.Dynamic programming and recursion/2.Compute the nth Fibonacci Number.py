3551acd526cb32f7db1de3a5b2da01f77085a56b
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-03 11:18:02 +0300

    [updated]: added more optimal solution

--- InterviewCake/6.Dynamic programming and recursion/2.Compute the nth Fibonacci Number.py
+++ InterviewCake/6.Dynamic programming and recursion/2.Compute the nth Fibonacci Number.py
@@ -1,12 +1,71 @@
 import unittest
 
 
-# my own solution
+# my own recursive solution
+# Time Complexity: O(2^n)
+# Space Complexity: O(2^n)
 def fib(n):
     if n in [0, 1]:
         return n
 
     return fib(n - 2) + fib(n - 1)
+
+
+# their bottom-up solution
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def fib(n):
+    # Edge cases:
+    if n < 0:
+        raise ValueError('Index was negative. No such thing as a '
+                         'negative index in a series.')
+    elif n in [0, 1]:
+        return n
+
+    # We'll be building the fibonacci series from the bottom up
+    # so we'll need to track the previous 2 numbers at each step
+    prev_prev = 0  # 0th fibonacci
+    prev = 1       # 1st fibonacci
+
+    for _ in range(n - 1):
+        # Iteration 1: current = 2nd fibonacci
+        # Iteration 2: current = 3rd fibonacci
+        # Iteration 3: current = 4th fibonacci
+        # To get nth fibonacci ... do n-1 iterations.
+        current = prev + prev_prev
+        prev_prev = prev
+        prev = current
+
+    return current
+
+
+# their memoization solution
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Fibber(object):
+
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n):
+        if n < 0:
+            # Edge case: negative index
+            raise ValueError('Index was negative. No such thing as a '
+                             'negative index in a series.')
+        elif n in [0, 1]:
+            # Base case: 0 or 1
+            return n
+
+        # See if we've already calculated this
+        if n in self.memo:
+            return self.memo[n]
+
+        result = self.fib(n - 1) + self.fib(n - 2)
+
+        # Memoize
+        self.memo[n] = result
+
+        return result
 
 
 class Test(unittest.TestCase):
