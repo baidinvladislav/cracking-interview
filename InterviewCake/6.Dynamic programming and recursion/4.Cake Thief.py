f24ecc52d615969393f31ca5d17ecc1a20fad3b1
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-12 23:31:43 +0300

    [updated]: added youtube solution

--- InterviewCake/6.Dynamic programming and recursion/4.Cake Thief.py
+++ InterviewCake/6.Dynamic programming and recursion/4.Cake Thief.py
@@ -1,6 +1,21 @@
 import unittest
 
 
+# https://youtu.be/jlCJqgSgXI4
+def knapsack(values, weights, k, lookup=None):
+    lookup = {} if not lookup else lookup
+    if k in lookup:
+        return lookup[k]
+
+    max_value = 0
+    for i in range(len(values)):
+        if weights[i] <= k:
+            max_value = max(max_value, values[i] + knapsack(values, weights, k - weights[i], lookup))
+
+    lookup[k] = max_value
+    return lookup[k]
+
+
 # their DP solution
 # Time Complexity: O(n * k)
 # Space Complexity: O(k)
@@ -47,47 +62,50 @@ def max_duffel_bag_value(cake_tuples, weight_capacity):
     return max_values_at_capacities[weight_capacity]
 
 
-class Test(unittest.TestCase):
-
-    def test_one_cake(self):
-        actual = max_duffel_bag_value([(2, 1)], 9)
-        expected = 4
-        self.assertEqual(actual, expected)
-
-    def test_two_cakes(self):
-        actual = max_duffel_bag_value([(4, 4), (5, 5)], 9)
-        expected = 9
-        self.assertEqual(actual, expected)
-
-    def test_only_take_less_valuable_cake(self):
-        actual = max_duffel_bag_value([(4, 4), (5, 5)], 12)
-        expected = 12
-        self.assertEqual(actual, expected)
-
-    def test_lots_of_cakes(self):
-        actual = max_duffel_bag_value([(2, 3), (3, 6), (5, 1), (6, 1), (7, 1), (8, 1)], 7)
-        expected = 12
-        self.assertEqual(actual, expected)
-
-    def test_value_to_weight_ratio_is_not_optimal(self):
-        actual = max_duffel_bag_value([(51, 52), (50, 50)], 100)
-        expected = 100
-        self.assertEqual(actual, expected)
-
-    def test_zero_capacity(self):
-        actual = max_duffel_bag_value([(1, 2)], 0)
-        expected = 0
-        self.assertEqual(actual, expected)
-
-    def test_cake_with_zero_value_and_weight(self):
-        actual = max_duffel_bag_value([(0, 0), (2, 1)], 7)
-        expected = 3
-        self.assertEqual(actual, expected)
-
-    def test_cake_with_non_zero_value_and_zero_weight(self):
-        actual = max_duffel_bag_value([(0, 5)], 5)
-        expected = float('inf')
-        self.assertEqual(actual, expected)
+print(knapsack(values=[4, 5], weights=[4, 5], k=12))
 
 
-unittest.main(verbosity=2)
+# class Test(unittest.TestCase):
+#
+#     def test_one_cake(self):
+#         actual = max_duffel_bag_value([(2, 1)], 9)
+#         expected = 4
+#         self.assertEqual(actual, expected)
+#
+#     def test_two_cakes(self):
+#         actual = max_duffel_bag_value([(4, 4), (5, 5)], 9)
+#         expected = 9
+#         self.assertEqual(actual, expected)
+#
+#     def test_only_take_less_valuable_cake(self):
+#         actual = max_duffel_bag_value([(4, 4), (5, 5)], 12)
+#         expected = 12
+#         self.assertEqual(actual, expected)
+#
+#     def test_lots_of_cakes(self):
+#         actual = max_duffel_bag_value([(2, 3), (3, 6), (5, 1), (6, 1), (7, 1), (8, 1)], 7)
+#         expected = 12
+#         self.assertEqual(actual, expected)
+#
+#     def test_value_to_weight_ratio_is_not_optimal(self):
+#         actual = max_duffel_bag_value([(51, 52), (50, 50)], 100)
+#         expected = 100
+#         self.assertEqual(actual, expected)
+#
+#     def test_zero_capacity(self):
+#         actual = max_duffel_bag_value([(1, 2)], 0)
+#         expected = 0
+#         self.assertEqual(actual, expected)
+#
+#     def test_cake_with_zero_value_and_weight(self):
+#         actual = max_duffel_bag_value([(0, 0), (2, 1)], 7)
+#         expected = 3
+#         self.assertEqual(actual, expected)
+#
+#     def test_cake_with_non_zero_value_and_zero_weight(self):
+#         actual = max_duffel_bag_value([(0, 5)], 5)
+#         expected = float('inf')
+#         self.assertEqual(actual, expected)
+
+
+# unittest.main(verbosity=2)
