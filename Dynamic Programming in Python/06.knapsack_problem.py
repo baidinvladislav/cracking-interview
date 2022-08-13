3433e152bae4f4f80adefbf930e779d6724f4392
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-13 17:18:13 +0300

    [updated]: repetition

--- Dynamic Programming in Python/06.knapsack_problem.py
+++ Dynamic Programming in Python/06.knapsack_problem.py
@@ -6,19 +6,24 @@ that form the highest cumulative price bounded by the capacity of the knapsack.
 
 # Solution 1: Simple recursion
 def solveKnapsack(weights, prices, capacity, index):
-    # base case of when we have run out of capacity or objects
+    # base case: either capacity already is 0 or pointer reached the end
     if capacity <= 0 or index >= len(weights):
         return 0
-    # if weight at index-th position is greater than capacity, skip this object
+
+    # check that we can put item to the bag
     if weights[index] > capacity:
-        return solveKnapsack_v1(weights, prices, capacity, index + 1)
-    # recursive call, either we can include the index-th object or we cannot, we check both possibilities and return the most optimal one using max
-    return max(prices[index] + solveKnapsack_v1(weights, prices, capacity - weights[index], index + 1),
-               solveKnapsack_v1(weights, prices, capacity, index + 1))
+        return solveKnapsack(weights, prices, capacity, index + 1)
+
+    # recursive call, either we can include the index-th object or we cannot,
+    # we check both possibilities and return the most optimal one using max
+    return max(
+        prices[index] + solveKnapsack(weights, prices, capacity - weights[index], index + 1),
+        solveKnapsack(weights, prices, capacity, index + 1)
+    )
 
 
 def knapsack(weights, prices, capacity):
-    return solveKnapsack_v1(weights, prices, capacity, 0)
+    return solveKnapsack(weights, prices, capacity, 0)
 
 
 print(knapsack([2, 1, 1, 3], [2, 8, 1, 10], 4))
