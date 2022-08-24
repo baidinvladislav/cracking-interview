9b2dc6210057fa10a62b91f0f56b473b09ab337c
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-24 18:20:17 +0300

    [updated]: I got it

--- InterviewCake/8.Linked lists/1.Delete Node.py
+++ InterviewCake/8.Linked lists/1.Delete Node.py
@@ -1,6 +1,18 @@
 import unittest
 
 
+# my identical solution
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+def delete_node(node_to_delete):
+    next_node = node_to_delete.next
+    if next_node:
+        node_to_delete.value = next_node.value
+        node_to_delete.next = next_node.next
+    else:
+        raise Exception('Can not delete the last node with this technique')
+
+
 # their solution
 # Time Complexity: O(1)
 # Space Complexity: O(1)
