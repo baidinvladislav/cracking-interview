693000f5bb5a1dabb9fda8351d25305eda8896e0
baydinvladislav <baydinvladislav@yandex.ru>
2022-08-27 13:42:37 +0300

    [created]: added their solution

--- InterviewCake/9.System design/3.Find Duplicate Files.py
+++ InterviewCake/9.System design/3.Find Duplicate Files.py
@@ -1,22 +1,50 @@
-def find_duplicates(files):
-    # init data structures
-    buffer = {}
+import os
+
+
+def find_duplicate_files(starting_directory):
+    files_seen_already = {}
+    stack = [starting_directory]
+
+    # We'll track tuples of (duplicate_file, original_file)
     duplicates = []
-    # looping through files
-    for current_file in files:
-        # add to store new element
-        if current_file not in buffer:
-            buffer[current_file] = current_file.time
-        # compare saved file with similar file
+
+    while len(stack) > 0:
+        current_path = stack.pop()
+
+        # If it's a directory,
+        # put the contents in our stack
+        if os.path.isdir(current_path):
+            for path in os.listdir(current_path):
+                full_path = os.path.join(current_path, path)
+                stack.append(full_path)
+
+        # If it's a file
         else:
-            # identify duplicate
-            if buffer[current_file].time < current_file.time:
-                duplicate = current_file
+            # Get its contents
+            with open(current_path) as file:
+                file_contents = file.read()
+
+            # Get its last edited time
+            current_last_edited_time = os.path.getmtime(current_path)
+
+            # If we've seen it before
+            if file_contents in files_seen_already:
+                existing_last_edited_time, existing_path = files_seen_already[file_contents]
+                if current_last_edited_time > existing_last_edited_time:
+                    # Current file is the dupe!
+                    duplicates.append((current_path, existing_path))
+                else:
+                    # Old file is the dupe!
+                    # So delete it
+                    duplicates.append((existing_path, current_path))
+                    # But also update files_seen_already to have
+                    # the new file's info
+                    files_seen_already[file_contents] = (current_last_edited_time, current_path)
+
+            # If it's a new file, throw it in files_seen_already
+            # and record the path and the last edited time,
+            # so we can delete it later if it's a dupe
             else:
-                duplicate = buffer[current_file]
+                files_seen_already[file_contents] = (current_last_edited_time, current_path)
 
-            # save duplicate
-            duplicates.append(duplicate)
-
-    # collected duplicates
     return duplicates
