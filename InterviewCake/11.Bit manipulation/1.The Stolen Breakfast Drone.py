a12607c5254437309c92bbf5dda3c54144e820ab
baydinvladislav <baydinvladislav@yandex.ru>
2022-09-04 15:56:54 +0300

    [updated]: added their solution

--- InterviewCake/11.Bit manipulation/1.The Stolen Breakfast Drone.py
+++ InterviewCake/11.Bit manipulation/1.The Stolen Breakfast Drone.py
@@ -1,6 +1,18 @@
 import unittest
 
 
+# their solution
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def find_unique_delivery_id(delivery_ids):
+    unique_delivery_id = 0
+
+    for delivery_id in delivery_ids:
+        unique_delivery_id ^= delivery_id
+
+    return unique_delivery_id
+
+
 # my solution
 # Time Complexity: O(n)
 # Space Complexity: O(n)
