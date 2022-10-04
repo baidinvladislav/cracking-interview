ed4428166828da03aee28d6edca8951f8329aca7
baydinvladislav <baydinvladislav@yandex.ru>
2022-10-04 21:09:47 +0300

    [update]: added O(n) solution

--- InterviewCake/3.Greedy algorithms/2.Highest Product of 3.py
+++ InterviewCake/3.Greedy algorithms/2.Highest Product of 3.py
@@ -1,6 +1,48 @@
 import unittest
 
 
+# my code based on their solution
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def highest_product_of_3(list_of_ints):
+    if len(list_of_ints) < 3:
+        raise ValueError('Less than 3 items!')
+
+    highest = max(list_of_ints[0], list_of_ints[1])
+    lowest = min(list_of_ints[0], list_of_ints[1])
+
+    highest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+    lowest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+
+    highest_product_of_3 = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
+
+    for i in range(2, len(list_of_ints)):
+        current = list_of_ints[i]
+
+        highest_product_of_3 = max(
+            highest_product_of_3,
+            current * highest_product_of_2,
+            current * lowest_product_of_2
+        )
+
+        highest_product_of_2 = max(
+            highest_product_of_2,
+            current * highest,
+            current * lowest
+        )
+
+        lowest_product_of_2 = min(
+            lowest_product_of_2,
+            current * highest,
+            current * lowest
+        )
+
+        highest = max(highest, current)
+        lowest = min(lowest, current)
+
+    return highest_product_of_3
+
+
 # my brute force solution
 # Time Complexity: O(n**3)
 # Space Complexity: O(1)
