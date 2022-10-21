c210b470c95a99a105e3edaaf10a68efa5921d5e
baydinvladislav <baydinvladislav@yandex.ru>
2022-10-21 17:24:23 +0300

    [updated]: added my code

--- Yandex Interview (important)/Problem #2.py
+++ Yandex Interview (important)/Problem #2.py
@@ -29,23 +29,23 @@ print(f('AAABBCF') == '3A2BCF')
 print(f('ABC') == 'ABC')
 
 
-# class Solution:
-#     def f(self, s):
-#         if not s:
-#             return 'на вход пришла невалидная строка'
-#
-#         s += '#'
-#         result = ''
-#         p1, p2 = 0, 1
-#
-#         while p2 != len(s):
-#             if s[p1] == s[p2]:
-#                 p2 += 1
-#             else:
-#                 result += f'{p2 - p1}{s[p1]}' if p2 - p1 != 1 else f'{s[p1]}'
-#                 p1 = p2
-#         return result
-#
-#
-# print(Solution().f(s='AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
-# print(Solution().f(s=''))
+class Solution:
+    def f(self, s):
+        if not s:
+            return 'на вход пришла невалидная строка'
+
+        s += '#'
+        result = ''
+        p1, p2 = 0, 1
+
+        while p2 != len(s):
+            if s[p1] == s[p2]:
+                p2 += 1
+            else:
+                result += f'{p2 - p1}{s[p1]}' if p2 - p1 != 1 else f'{s[p1]}'
+                p1 = p2
+        return result
+
+
+print(Solution().f(s='AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
+print(Solution().f(s=''))
