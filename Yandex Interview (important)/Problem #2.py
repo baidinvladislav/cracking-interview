cacd28ea96d48e52370531393469910f1ee89255
baydinvladislav <baydinvladislav@yandex.ru>
2022-10-21 17:24:00 +0300

    [updated]: added my code

--- Yandex Interview (important)/Problem #2.py
+++ Yandex Interview (important)/Problem #2.py
@@ -7,23 +7,45 @@
 """
 
 
-class Solution:
-    def f(self, s):
-        if not s:
-            return 'на вход пришла невалидная строка'
-
-        s += '#'
-        result = ''
-        p1, p2 = 0, 1
-
-        while p2 != len(s):
-            if s[p1] == s[p2]:
-                p2 += 1
+def f(string):
+    string += '#'
+    counter = 0
+    result = ''
+    for i in range(1, len(string)):
+        if string[i - 1] == string[i]:
+            counter += 1
+        else:
+            if counter == 0:
+                result += string[i - 1]
             else:
-                result += f'{p2 - p1}{s[p1]}' if p2 - p1 != 1 else f'{s[p1]}'
-                p1 = p2
-        return result
+                result += f'{counter + 1}{string[i - 1]}'
+                counter = 0
+
+    return result
 
 
-print(Solution().f(s='AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
-print(Solution().f(s=''))
+print(f('AAABBCCF') == '3A2B2CF')
+print(f('AAABBCF') == '3A2BCF')
+print(f('ABC') == 'ABC')
+
+
+# class Solution:
+#     def f(self, s):
+#         if not s:
+#             return 'на вход пришла невалидная строка'
+#
+#         s += '#'
+#         result = ''
+#         p1, p2 = 0, 1
+#
+#         while p2 != len(s):
+#             if s[p1] == s[p2]:
+#                 p2 += 1
+#             else:
+#                 result += f'{p2 - p1}{s[p1]}' if p2 - p1 != 1 else f'{s[p1]}'
+#                 p1 = p2
+#         return result
+#
+#
+# print(Solution().f(s='AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
+# print(Solution().f(s=''))
