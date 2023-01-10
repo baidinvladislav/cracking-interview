2c6f668b5e1dc279213fa6aa2b26a5064aadf34d
baydinvladislav <baydinvladislav@yandex.ru>
2023-01-10 17:24:53 +0300

    [updated]: repeat maxProfit

--- Best Practice Questions/Problems/Week 1 - Sequences/121. Best Time to Buy and Sell Stock.py
+++ Best Practice Questions/Problems/Week 1 - Sequences/121. Best Time to Buy and Sell Stock.py
@@ -1,15 +1,14 @@
 import unittest
+from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices):
-        max_profit, min_price = 0, float('inf')
-
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price, min_price = 0, float("inf")
         for price in prices:
             min_price = min(min_price, price)
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
-        return max_profit
+            max_price = max(max_price, price - min_price)
+        return max_price
 
 
 class TestBestTimeBuyAndSellStock(unittest.TestCase):
