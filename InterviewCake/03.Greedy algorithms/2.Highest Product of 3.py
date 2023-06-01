f68830345ea1acd61b854e531801d1aecd42c27d
baydinvladislav <baydinvladislav@yandex.ru>
2023-06-01 17:52:36 +0300

    interviewCake repetition: 3w1d

--- InterviewCake/03.Greedy algorithms/2.Highest Product of 3.py
+++ InterviewCake/03.Greedy algorithms/2.Highest Product of 3.py
@@ -5,42 +5,37 @@ import unittest
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 def highest_product_of_3(list_of_ints):
-    if len(list_of_ints) < 3:
-        raise ValueError('Less than 3 items!')
-
     highest = max(list_of_ints[0], list_of_ints[1])
     lowest = min(list_of_ints[0], list_of_ints[1])
 
-    highest_product_of_2 = list_of_ints[0] * list_of_ints[1]
-    lowest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+    highest_of_2 = list_of_ints[0] * list_of_ints[1]
+    lowest_of_2 = list_of_ints[0] * list_of_ints[1]
 
-    highest_product_of_3 = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
+    highest_of_3 = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
 
     for i in range(2, len(list_of_ints)):
-        current = list_of_ints[i]
-
-        highest_product_of_3 = max(
-            highest_product_of_3,
-            current * highest_product_of_2,
-            current * lowest_product_of_2
+        highest_of_3 = max(
+            highest_of_3,
+            highest_of_2 * list_of_ints[i],
+            lowest_of_2 * list_of_ints[i]
         )
 
-        highest_product_of_2 = max(
-            highest_product_of_2,
-            current * highest,
-            current * lowest
+        highest_of_2 = max(
+            highest_of_2,
+            highest * list_of_ints[i],
+            lowest * list_of_ints[i]
         )
 
-        lowest_product_of_2 = min(
-            lowest_product_of_2,
-            current * highest,
-            current * lowest
+        lowest_of_2 = min(
+            lowest_of_2,
+            highest * list_of_ints[i],
+            lowest * list_of_ints[i]
         )
 
-        highest = max(highest, current)
-        lowest = min(lowest, current)
+        highest = max(highest, list_of_ints[i])
+        lowest = max(lowest, list_of_ints[i])
 
-    return highest_product_of_3
+    return highest_of_3
 
 
 # my brute force solution
