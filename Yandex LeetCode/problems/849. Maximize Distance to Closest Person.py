503567dd3330a63bcfa1092c62ca8d57b3816ce6
baydinvladislav <baydinvladislav@yandex.ru>
2024-05-14 17:08:53 +0300

    update 849. Maximize Distance to Closest Person

--- Yandex LeetCode/problems/849. Maximize Distance to Closest Person.py
+++ Yandex LeetCode/problems/849. Maximize Distance to Closest Person.py
@@ -1,28 +1,36 @@
+from typing import List
+
+
 class Solution:
-    def maxDistToClosest(self, seats):
-        reserved = (i for i, seat in enumerate(seats) if seat == 1)
-        prev = None
-        future = next(reserved)
-        result = 0
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        # Инициализируем переменную для хранения максимального расстояния
+        max_distance = 0
+        # Инициализируем переменную для хранения индекса последнего встреченного человека
+        last_person = -1
+        # Длина массива seats
+        n = len(seats)
 
-        for i, seat in enumerate(seats):
-            if seat:
-                prev = i
-            else:
-                while future is not None and future < i:
-                    future = next(reserved, None)
+        # Проходим по каждому элементу массива seats
+        for i in range(n):
+            # Если текущий элемент - человек (1)
+            if seats[i] == 1:
+                # Если это первый встреченный человек
+                if last_person == -1:
+                    # Максимальное расстояние до ближайшего человека для всех предыдущих пустых мест равно i
+                    max_distance = i
+                else:
+                    # Если это не первый человек, вычисляем расстояние до ближайшего человека для всех мест между last_person и текущим человеком
+                    max_distance = max(max_distance, (i - last_person) // 2)
+                # Обновляем last_person на текущий индекс
+                last_person = i
 
-                left = float('inf') if prev is None else i - prev
-                right = float('inf') if future is None else future - i
-                result = max(result, min(left, right))
+        # Обработка хвоста массива, если последние места пустые
+        if seats[n - 1] == 0:
+            # Проверяем и обновляем max_distance, если расстояние от последнего человека до конца массива больше текущего max_distance
+            max_distance = max(max_distance, n - 1 - last_person)
 
-        return result
-
-
-def main():
-    print(Solution().maxDistToClosest(seats=[1, 0, 0, 0, 1, 0, 1]))
-    print(Solution().maxDistToClosest(seats=[1, 0, 0, 0]))
-    print(Solution().maxDistToClosest(seats=[0, 1]))
+        # Возвращаем максимальное расстояние до ближайшего человека
+        return max_distance
 
 
-main()
+Solution().maxDistToClosest(seats=[1, 0, 0, 0, 1, 0, 1])
